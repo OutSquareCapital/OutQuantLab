a4e8ced85036d32f8bc49fc18948219e1cdcbb11
OutSquareCapital <outsquarecapital@gmail.com>
2024-12-24 19:50:14 +0100

    big update: UI and Backend aare now completely independents

---
+++ App_Backend.py
@@ -0,0 +1,41 @@
+from Files import FILE_PATH_YF
+from Get_Data import get_yahoo_finance_data
+from Backtest import BacktestProcess
+from Portfolio import aggregate_raw_returns
+from Config import AssetsCollection, IndicatorsCollection
+from Dashboard import DashboardsCollection
+from collections.abc import Callable
+
+def handle_progress(progress: int, message: str):
+    print(f"[{progress}%] {message}")
+
+class OutQuantLab:
+    def __init__(self, progress_callback: Callable):
+        self.assets_collection = AssetsCollection()
+        self.indicators_collection = IndicatorsCollection()
+        self.dashboards = DashboardsCollection(length=1250)
+        self.backtest_process = BacktestProcess(
+            file_path=FILE_PATH_YF,
+            asset_names=self.assets_collection.get_active_entities_names(),
+            asset_clusters=self.assets_collection.clusters,
+            indics_clusters=self.indicators_collection.clusters,
+            indicators_and_params=self.indicators_collection.get_indicators_and_parameters_for_backtest(),
+            progress_callback=progress_callback
+            )
+
+    def run_backtest(self):
+        raw_adjusted_returns_df = self.backtest_process.calculate_strategy_returns()
+        self.dashboards.global_portfolio, self.dashboards.sub_portfolios = aggregate_raw_returns(raw_adjusted_returns_df)
+
+    def refresh_data(self):
+        get_yahoo_finance_data(self.assets_collection.get_all_entities_names(), FILE_PATH_YF)
+
+    def close(self):
+        self.assets_collection.save()
+        self.indicators_collection.save()
+        
+if __name__ == "__main__":
+    outquantlab = OutQuantLab(handle_progress)
+    outquantlab.refresh_data()
+    outquantlab.run_backtest()
+    print(outquantlab.dashboards.metrics)
---
+++ App_Frontend.py
@@ -0,0 +1,65 @@
+from PySide6.QtWidgets import QMainWindow, QProgressBar, QTextEdit
+
+class MainApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+    def initialize(self):
+        self.progress_bar:QProgressBar
+        self.log_output:QTextEdit
+        self.outquantlab = OutQuantLab(self.update_progress)
+        self.show_home_page()
+        self.showMaximized()
+
+    def show_home_page(self):
+        UI.setup_home_page(
+        parent=self,
+        run_backtest_callback=self.run_backtest,
+        refresh_data_callback=self.outquantlab.refresh_data,
+        assets_collection=self.outquantlab.assets_collection,
+        indicators_collection=self.outquantlab.indicators_collection
+        )
+
+    def update_progress(self, value, message):
+        UI.update_progress_with_events(self.progress_bar, self.log_output, value, message)
+
+
+    def run_backtest(self):
+        self.progress_bar, self.log_output = UI.setup_backtest_page(self)
+        self.outquantlab.run_backtest()
+        self.show_results_page()
+
+    def show_results_page(self):
+        self.outquantlab.dashboards.metrics = self.outquantlab.dashboards.calculate_metrics()
+
+        UI.setup_results_page(
+        parent=self,
+        dashboards=self.outquantlab.dashboards,
+        back_to_home_callback=self.show_home_page,
+        metrics=self.outquantlab.dashboards.metrics
+        )
+
+    def closeEvent(self, event):
+        self.outquantlab.close()
+        UI.cleanup_temp_files()
+        super().closeEvent(event)
+
+if __name__ == "__main__":
+
+    import sys
+    from PySide6.QtWidgets import QApplication
+    import UI
+    app = QApplication(sys.argv)
+    UI.apply_global_styles(app)
+    progress_window, progress_bar = UI.setup_launch_page(None)
+
+    QApplication.processEvents()
+    progress_bar.setValue(30)
+    from App_Backend import OutQuantLab
+    main_window = MainApp()
+    progress_bar.setValue(100)
+
+    main_window.initialize()
+    progress_window.close()
+    
+    sys.exit(app.exec())
