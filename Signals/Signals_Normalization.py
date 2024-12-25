dc47f4169652ac94186123581e7e3d1789c84e34
OutSquareCapital <outsquarecapital@gmail.com>
2024-12-25 17:25:12 +0100

    typing update

--- Signals/Signals_Normalization.py
+++ Signals/Signals_Normalization.py
@@ -21,29 +21,47 @@ def calculate_indicator_on_trend_signal(trend_signal: NDArray[np.float32], indic
         indicator_signal
     )
 
-def rolling_median_normalisation(signal_array: NDArray[np.float32], window_length: int) -> NDArray[np.float32]:
-    limit = 1
-    median_array = rolling_median(signal_array, length=window_length, min_length=window_length)
-    min = rolling_min(signal_array, length=window_length, min_length=window_length)
-    max = rolling_max(signal_array, length=window_length, min_length=window_length)
+def rolling_median_normalisation(
+    signal_array: NDArray[np.float32], 
+    window_length: int, 
+    limit:int = 1
+    ) -> NDArray[np.float32]:
+
     adjusted_signal_array = np.empty_like(signal_array, dtype=np.float32)
-    ne.evaluate("((signal_array - median_array) / (rolling_max - rolling_min)) * 2", out=adjusted_signal_array)
+    dict = {
+        "signal_array": signal_array,
+        "median_array": rolling_median(signal_array, length=window_length, min_length=window_length),
+        "max": rolling_min(signal_array, length=window_length, min_length=window_length),
+        "min": rolling_max(signal_array, length=window_length, min_length=window_length)
+    }
+    ne.evaluate("((signal_array - median_array) / (max - min)) * 2", out=adjusted_signal_array, local_dict=dict)
+
     return np.clip(adjusted_signal_array, -limit, limit)
 
-def rolling_std_normalisation(signal_array: NDArray[np.float32], window_length: int) -> NDArray[np.float32]:
-    limit = 1
-    median_array = rolling_median(signal_array, length=window_length, min_length=window_length)
-    rolling_std = rolling_volatility(signal_array, length=window_length, min_length=window_length)
+def rolling_std_normalisation(
+    signal_array: NDArray[np.float32], 
+    window_length: int, 
+    limit:int = 1
+    ) -> NDArray[np.float32]:
+
     adjusted_signal_array = np.empty_like(signal_array, dtype=np.float32)
-    ne.evaluate("((signal_array - median_array) / rolling_std) * 0.5", out=adjusted_signal_array)
+    dict = {
+        "signal_array": signal_array,
+        "median_array": rolling_median(signal_array, length=window_length, min_length=window_length),
+        "rolling_std": rolling_volatility(signal_array, length=window_length, min_length=window_length)
+    }
+    ne.evaluate("((signal_array - median_array) / rolling_std) * 0.5", out=adjusted_signal_array, local_dict=dict)
+    
     return np.clip(adjusted_signal_array, -limit, limit)
 
-def rolling_scalar_normalisation(signal_array: NDArray[np.float32], window_length: int) -> NDArray[np.float32]:
-    scalar = 1
-    limit = 2
+def rolling_scalar_normalisation(
+    signal_array: NDArray[np.float32], 
+    window_length: int, 
+    scalar:int = 1, 
+    limit:int = 2
+    ) -> NDArray[np.float32]:
     row_mean_array = np.nanmean(np.abs(signal_array), axis=1)
     median = rolling_median(row_mean_array, length=window_length, min_length=1)
     median = bfill(median)
-    adjusted_signal_array = np.empty_like(signal_array, dtype=np.float32)
     normalization_factor = scalar / median[:, None]
     return np.clip(signal_array * normalization_factor, -limit, limit)
