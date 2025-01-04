5504f79fe3cbce0b3476cf2567c534b36348142b
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-04 20:54:04 +0100

    oops

--- Run.py
+++ Run.py
@@ -2,8 +2,7 @@ if __name__ == "__main__":
     print(f'initializing OutQuantLab...')
     from sys import exit
     from App import OutQuantLabCLI, OutQuantLabGUI
-    choice = '1'
-    #choice = input("Enter 1 to run CLI, 2 to run GUI: ")
+    choice = input("Enter 1 to run CLI, 2 to run GUI: ")
     if choice == '1':
         OutQuantLabCLI()
         exit(0)
