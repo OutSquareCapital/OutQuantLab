bugfixe max dd, started reintegration of sharpe opti + fixed his median calculation

Backtest/Sharpe_Optimization.py
@@ -1,28 +1,31 @@
 from Utilitary import ArrayFloat, Float32
 import numpy as np
-from Metrics import rolling_sharpe_ratios, rolling_mean
+from Metrics import rolling_sharpe_ratios, shift_array
+
+def broadcast_median(array: ArrayFloat) -> ArrayFloat:
+    medians = np.nanmedian(array, axis=1)
+    return np.tile(medians[:, np.newaxis], (1, array.shape[1]))
 
 def relative_sharpe_on_confidence_period(
     returns_array: ArrayFloat,
-    sharpe_lookback:int, 
     confidence_lookback: int = 2500
     ) -> ArrayFloat:
 
     def count_non_nan(x: ArrayFloat) -> ArrayFloat:
         return np.cumsum(~np.isnan(x), axis=0, dtype=Float32)
 
-    sharpe_array: ArrayFloat =rolling_sharpe_ratios(
+    sharpe_array: ArrayFloat = rolling_sharpe_ratios(
         returns_array=returns_array, 
-        length = sharpe_lookback, 
-        min_length = 1
+        length = returns_array.shape[0], 
+        min_length = 20
         )
-    mean_sharpe_array: ArrayFloat = rolling_mean( array=sharpe_array, length=20, min_length=1)
-    non_nan_counts: ArrayFloat = count_non_nan(mean_sharpe_array)
-    rolling_median_sharpe = np.nanmedian(mean_sharpe_array, axis=1)
-    
+    non_nan_counts: ArrayFloat = count_non_nan(sharpe_array)
+    rolling_median_sharpe: ArrayFloat = broadcast_median(array=sharpe_array)
     normalized_sharpes: ArrayFloat = (
-        mean_sharpe_array - rolling_median_sharpe[:, np.newaxis]
+        sharpe_array - rolling_median_sharpe
         ) * (
             (non_nan_counts / confidence_lookback)**0.5
             ) + 1
-    return np.clip(normalized_sharpes, 0, None)
+        
+    clipped_sharpes = np.clip(normalized_sharpes, 0, 2)
+    return returns_array * shift_array(returns_array=clipped_sharpes)