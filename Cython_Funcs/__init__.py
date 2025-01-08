b4d99adf1bd03ca756dade71e045209fb8d40cb2
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-08 20:04:20 +0100

    refactor: remove unused Cython functions and clean up requirements

--- Cython_Funcs/__init__.py
+++
@@ -1,3 +0,0 @@
-from Cython_Funcs.funcs import fill_signals_array # type: ignore
-
-__all__: list[str] = ["fill_signals_array"]
--- Cython_Funcs/funcs.pyi
+++
@@ -1,7 +0,0 @@
-from Utilitary import ArrayFloat
-def fill_signals_array(
-    signals_array: ArrayFloat,
-    results: list[ArrayFloat],
-    total_assets_count: int,
-    start_index: int
-) -> int: ...
