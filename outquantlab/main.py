2c842a2e686a1f058be96582f869590a5b87e128
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-26 20:24:57 +0100

    refactor main.py to replace Backtester with execute_backtest for improved clarity and data handling

--- outquantlab/main.py
+++ outquantlab/main.py
@@ -1,4 +1,4 @@
-from outquantlab.backtest import Backtester
+from outquantlab.backtest import execute_backtest
 from outquantlab.config_classes import (
     AssetsClusters,
     AssetsCollection,
@@ -13,22 +13,22 @@ from outquantlab.indicators import DataDfs
 class OutQuantLab:
     def __init__(self) -> None:
         self.dbp = DataBaseProvider()
-        self.data_dfs: DataDfs = self.dbp.get_initial_data()
         self.assets_collection: AssetsCollection = self.dbp.get_assets_collection()
         self.indics_collection: IndicsCollection = self.dbp.get_indics_collection()
         self.assets_clusters: AssetsClusters = self.dbp.get_assets_clusters_tree()
         self.indics_clusters: IndicsClusters = self.dbp.get_indics_clusters_tree()
 
     def run(self) -> GraphsCollection:
-        Backtester(
-            data_dfs=self.data_dfs,
+        data_dfs: DataDfs = self.dbp.get_initial_data()
+        execute_backtest(
+            data_dfs=data_dfs,
             indics_params=self.indics_collection.get_indics_params(),
             assets=self.assets_collection.get_all_active_entities(),
             indics_clusters=self.indics_clusters,
             assets_clusters=self.assets_clusters,
         )
-
-        return GraphsCollection(data_dfs=self.data_dfs)
+        print(data_dfs.global_returns)
+        return GraphsCollection(data_dfs=data_dfs)
 
     def save_all(self) -> None:
         self.dbp.save_all(
