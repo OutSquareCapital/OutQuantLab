5fd765e4f2c5301e97435397bc9e20fb39388334
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-26 05:39:14 +0100

    better handling of progress statut

--- outquantlab/backtest/progress_statut.py
+++ outquantlab/backtest/progress_statut.py
@@ -1,24 +1,26 @@
 class ProgressStatus:
-    def __init__(self) -> None:
+    def __init__(self, total_returns_streams: int, clusters_nb:int) -> None:
         self.current_progress: int = 0
-
+        self.total_returns_streams: int = total_returns_streams
+        self.clusters_nb: int = clusters_nb
+    
     def progress_callback(self, message: str) -> None:
         print(f"[{self.current_progress}%] {message}")
 
     def get_strategies_process_progress(
-        self, indic_name: str, strategies_nb:int, signal_col_index: int, total_returns_streams: int
+        self, signal_col_index: int
     ) -> None:
-        self.current_progress = int(50 * signal_col_index / total_returns_streams)
-        self.progress_callback(message=f"Processing {indic_name} with {strategies_nb} strategies...")
+        self.current_progress = int(50 * signal_col_index / self.total_returns_streams)
+        self.progress_callback(message="Processing strategies...")
 
     def get_aggregation_progress(
-        self, i: int, clusters_nb: int, current_lvl: str, columns_left: int
+        self, i: int
     ) -> None:
-        self.current_progress = 50 + int(50 * (clusters_nb - i + 1) / clusters_nb)
+        self.current_progress = 50 + int(50 * (self.clusters_nb - i + 1) / self.clusters_nb)
 
         if self.current_progress == 100:
             self.progress_callback(message="Backtest Completed!")
         else:
             self.progress_callback(
-                message=f"Aggregating {current_lvl}: {columns_left} columns left..."
+                message="Aggregating returns..."
             )
