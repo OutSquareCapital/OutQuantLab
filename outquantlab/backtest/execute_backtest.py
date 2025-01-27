40de2f4b82de853abf1c238a58daa17e31b354ce
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-27 17:51:05 +0100

    encapsulated almost all backtest related variables into BacktestConfig (from ClustersIndex rename)

--- outquantlab/backtest/execute_backtest.py
+++ outquantlab/backtest/execute_backtest.py
@@ -1,66 +1,74 @@
-from outquantlab.backtest.aggregate_returns import aggregate_raw_returns
+from outquantlab.backtest.aggregate_returns import (
+    calculate_portfolio_returns,
+    get_global_portfolio_returns,
+)
 from outquantlab.backtest.process_strategies import (
-    get_signals_array,
     process_strategies,
 )
-from outquantlab.config_classes import ProgressStatus
 from outquantlab.config_classes import (
-    Asset,
+    BacktestConfig,
     ConfigState,
-    ClustersIndex,
 )
-from outquantlab.indicators import BaseIndic, DataArrays, DataDfs
+from outquantlab.graphs import GraphsCollection
+from outquantlab.indicators import DataDfs
 from outquantlab.typing_conventions import ArrayFloat, DataFrameFloat
 
 
 def execute_backtest(
-    data_dfs: DataDfs,
-    config: ConfigState
-) -> None:
-    indics_params: list[BaseIndic]=config.indics_collection.get_indics_params()
-    assets: list[Asset]=config.assets_collection.get_all_active_entities()
-    
-    clusters_index: ClustersIndex = config.generate_multi_index_process(
-        indics_params=indics_params,
-        assets=assets
-    )
+    returns_df: DataFrameFloat, config: ConfigState
+) -> GraphsCollection:
+    data_dfs: DataDfs = DataDfs(returns_df=returns_df)
 
-    progress: ProgressStatus = clusters_index.get_progress()
+    backtest_config: BacktestConfig = config.generate_multi_index_process()
 
     get_backtest_returns(
         data_dfs=data_dfs,
-        indics_params=indics_params,
-        clusters_index=clusters_index,
-        progress=progress,
+        backtest_config=backtest_config,
     )
     aggregate_raw_returns(
         data_dfs=data_dfs,
-        clusters_nb=clusters_index.clusters_nb,
-        clusters_names=clusters_index.clusters_names,
-        progress=progress,
+        backtest_config=backtest_config,
     )
+    print(data_dfs.global_returns)
+    return GraphsCollection(data_dfs=data_dfs)
 
 
 def get_backtest_returns(
     data_dfs: DataDfs,
-    indics_params: list[BaseIndic],
-    clusters_index: ClustersIndex,
-    progress: ProgressStatus,
+    backtest_config: BacktestConfig,
 ) -> None:
-    data_arrays: DataArrays = data_dfs.select_data()
-    signals_array: ArrayFloat = get_signals_array(
-        total_returns_streams=clusters_index.total_returns_streams,
-        observations_nb=data_arrays.prices_array.shape[0],
-    )
-    signals_array = process_strategies(
-        signals_array=signals_array,
-        data_arrays=data_arrays,
-        indics_params=indics_params,
-        assets_count=clusters_index.assets_nb,
-        progress=progress,
+    signals_array: ArrayFloat = process_strategies(
+        data_arrays=data_dfs.select_data(),
+        backtest_config=backtest_config,
     )
     data_dfs.global_returns = DataFrameFloat(
         data=signals_array,
         index=data_dfs.global_returns.dates,
-        columns=clusters_index.multi_index,
+        columns=backtest_config.multi_index,
+    )
+
+
+def aggregate_raw_returns(
+    data_dfs: DataDfs,
+    backtest_config: BacktestConfig,
+) -> None:
+    for lvl in range(backtest_config.clusters_nb, 0, -1):
+        data_dfs.global_returns = calculate_portfolio_returns(
+            returns_df=data_dfs.global_returns,
+            grouping_levels=backtest_config.clusters_names[:lvl],
+        )
+        if lvl == 5:
+            data_dfs.global_returns.dropna(axis=0, how="any", inplace=True)  # type: ignore
+            data_dfs.sub_portfolio_ovrll = DataFrameFloat(data=data_dfs.global_returns)
+
+        if lvl == 2:
+            data_dfs.global_returns.dropna(axis=0, how="all", inplace=True)  # type: ignore
+            data_dfs.sub_portfolio_roll = DataFrameFloat(data=data_dfs.global_returns)
+
+        backtest_config.progress.get_aggregation_progress(lvl=lvl)
+
+    data_dfs.global_returns.dropna(axis=0, how="all", inplace=True)  # type: ignore
+
+    data_dfs.global_returns = get_global_portfolio_returns(
+        returns_df=data_dfs.global_returns
     )
--- outquantlab/config_classes/config_state.py
+++ outquantlab/config_classes/config_state.py
@@ -1,31 +1,35 @@
-from outquantlab.config_classes.collections import IndicsCollection, AssetsCollection
+from dataclasses import dataclass, field
+
+from pandas import MultiIndex
+
 from outquantlab.config_classes.clusters import (
     AssetsClusters,
-    IndicsClusters,
-    Asset,
     BaseIndic,
+    IndicsClusters,
     generate_levels,
 )
-from dataclasses import dataclass
+from outquantlab.config_classes.collections import AssetsCollection, IndicsCollection
 from outquantlab.config_classes.progress_statut import ProgressStatus
-from pandas import MultiIndex
 
 
 @dataclass(slots=True)
-class ClustersIndex:
+class BacktestConfig:
     multi_index: MultiIndex
+    indics_params: list[BaseIndic]
     assets_nb: int
     clusters_names: list[str]
     total_returns_streams: int
     clusters_nb: int
+    progress: ProgressStatus = field(init=False)
 
-    def get_progress(self) -> ProgressStatus:
-        return ProgressStatus(
+    def __post_init__(self) -> None:
+        self.progress = ProgressStatus(
             total_returns_streams=self.total_returns_streams,
             clusters_nb=self.clusters_nb,
         )
 
-@dataclass()
+
+@dataclass(slots=True)
 class ConfigState:
     indics_collection: IndicsCollection
     assets_collection: AssetsCollection
@@ -34,11 +38,11 @@ class ConfigState:
 
     def generate_multi_index_process(
         self,
-        indics_params: list[BaseIndic],
-        assets: list[Asset],
-    ) -> ClustersIndex:
+    ) -> BacktestConfig:
+        indics_params: list[BaseIndic] = self.indics_collection.get_indics_params()
+    
         asset_tuples: list[tuple[str, ...]] = self.assets_clusters.get_clusters_tuples(
-            entities=assets
+            entities=self.assets_collection.get_all_active_entities()
         )
         indics_tuples: list[tuple[str, ...]] = self.indics_clusters.get_clusters_tuples(
             entities=indics_params
@@ -53,8 +57,9 @@ class ConfigState:
             tuples=product_tuples,
             names=generate_levels(num_levels=num_levels),
         )
-        return ClustersIndex(
+        return BacktestConfig(
             multi_index=multi_index,
+            indics_params=indics_params,
             assets_nb=len(asset_tuples),
             clusters_names=multi_index.names,
             total_returns_streams=len(multi_index),
