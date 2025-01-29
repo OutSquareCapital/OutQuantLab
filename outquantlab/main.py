eb4f3099b0266c36bd9fc0f8da73a95404a6c1e7
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-29 17:19:16 +0100

    uncomplete commit

--- outquantlab/main.py
+++ outquantlab/main.py
@@ -1,21 +1,23 @@
 from outquantlab.backtest import execute_backtest
-from outquantlab.config_classes import AppConfig
+from outquantlab.config_classes import AppConfig, get_backtest_config
 from outquantlab.database import DataBaseProvider
-from outquantlab.graphs import GraphsCollection
-
+from outquantlab.typing_conventions import DataFrameFloat
+import outquantlab.graphs as graphs
 
 class OutQuantLab:
     def __init__(self) -> None:
-        self.dbp = DataBaseProvider()
-        self.app_config: AppConfig = self.dbp.get_config()
+        self._dbp = DataBaseProvider()
+        self.app_config: AppConfig = self._dbp.get_config()
+        self.data: dict[str, DataFrameFloat] = {}
+        self.graphs = graphs
 
-    def run(self) -> GraphsCollection:
-        return execute_backtest(
-            data_dfs=self.dbp.get_data(
+    def run(self) -> None:
+        self.data = execute_backtest(
+            returns_df=self._dbp.get_data(
                 names=self.app_config.assets_config.get_all_active_entities_names()
             ),
-            backtest_config=self.app_config.get_backtest_config(),
+            backtest_config=get_backtest_config(app_config=self.app_config),
         )
 
     def save(self) -> None:
-        self.dbp.save_config(config=self.app_config)
+        self._dbp.save_config(config=self.app_config)
