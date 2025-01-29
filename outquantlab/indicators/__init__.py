8ab9a5784365e6e7f241b4851dfa32dd93dd1256
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-29 18:05:21 +0100

    centralized BaseIndics and DataArrayas into module indics_config

--- outquantlab/indicators/__init__.py
+++ outquantlab/indicators/__init__.py
@@ -1,9 +1,8 @@
-from outquantlab.indicators.indics_normalized import IndicsNormalized, BaseIndic
-from outquantlab.indicators.indics_data import DataArrays, get_data_arrays
-
+from outquantlab.indicators.indics_config import BaseIndic, DataArrays, get_data_arrays
+from outquantlab.indicators.indics_normalized import IndicsNormalized
 __all__: list[str] = [
     'DataArrays',
     'BaseIndic',
+    'get_data_arrays',
     'IndicsNormalized',
-    'get_data_arrays'
 ]
