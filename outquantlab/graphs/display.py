c030d9c2a0b4a4270df81f200b206dd2e8d355bd
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-29 17:06:06 +0100

    deleted graphcollections class in favor of pures funcs, for greater flexibility and decoupling

--- outquantlab/graphs/display.py
+++ outquantlab/graphs/display.py
@@ -2,171 +2,179 @@ import plotly.graph_objects as go  # type: ignore
 
 import outquantlab.graphs.widgets as widgets
 import outquantlab.stats as stats
-from outquantlab.indicators import DataDfs
+from outquantlab.typing_conventions import DataFrameFloat
 
 
 def format_plot_name(name: str) -> str:
     return name.replace("plot", "").replace("_", " ").title()
 
 
-class GraphsCollection:
-    def __init__(self, data_dfs: DataDfs) -> None:
-        self.data_dfs: DataDfs = data_dfs
+def plot_stats_equity(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.curves(
+        returns_df=stats.get_stats_equity(returns_df=returns_df),
+        title=format_plot_name(name=plot_stats_equity.__name__),
+        log_scale=True,
+        show_legend=show_legend,
+    )
 
-    def plot_stats_equity(self, show_legend: bool = True) -> go.Figure:
-        return widgets.curves(
-            returns_df=stats.get_stats_equity(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_stats_equity.__name__),
-            log_scale=True,
-            show_legend=show_legend,
-        )
 
-    def plot_rolling_volatility(self, show_legend: bool = True) -> go.Figure:
-        return widgets.curves(
-            returns_df=stats.get_rolling_volatility(
-                returns_df=self.data_dfs.sub_portfolio_roll
-            ),
-            title=format_plot_name(name=self.plot_rolling_volatility.__name__),
-            show_legend=show_legend,
-        )
+def plot_rolling_volatility(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.curves(
+        returns_df=stats.get_rolling_volatility(returns_df=returns_df),
+        title=format_plot_name(name=plot_rolling_volatility.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_rolling_drawdown(self, length: int, show_legend: bool = True) -> go.Figure:
-        return widgets.curves(
-            returns_df=stats.get_rolling_drawdown(
-                returns_df=self.data_dfs.sub_portfolio_roll, length=length
-            ),
-            title=format_plot_name(name=self.plot_rolling_drawdown.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_rolling_sharpe_ratio(
-        self, length: int, show_legend: bool = True
-    ) -> go.Figure:
-        return widgets.curves(
-            returns_df=stats.get_rolling_sharpe_ratio(
-                returns_df=self.data_dfs.sub_portfolio_roll, length=length
-            ),
-            title=format_plot_name(name=self.plot_rolling_sharpe_ratio.__name__),
-            show_legend=show_legend,
-        )
+def plot_rolling_drawdown(
+    returns_df: DataFrameFloat, length: int, show_legend: bool = True
+) -> go.Figure:
+    return widgets.curves(
+        returns_df=stats.get_rolling_drawdown(returns_df=returns_df, length=length),
+        title=format_plot_name(name=plot_rolling_drawdown.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_rolling_smoothed_skewness(
-        self, length: int, show_legend: bool = True
-    ) -> go.Figure:
-        return widgets.curves(
-            returns_df=stats.get_rolling_smoothed_skewness(
-                returns_df=self.data_dfs.sub_portfolio_roll, length=length
-            ),
-            title=format_plot_name(name=self.plot_rolling_smoothed_skewness.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_overall_returns(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_returns(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_returns.__name__),
-            show_legend=show_legend,
-        )
+def plot_rolling_sharpe_ratio(
+    returns_df: DataFrameFloat, length: int, show_legend: bool = True
+) -> go.Figure:
+    return widgets.curves(
+        returns_df=stats.get_rolling_sharpe_ratio(returns_df=returns_df, length=length),
+        title=format_plot_name(name=plot_rolling_sharpe_ratio.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_overall_sharpe_ratio(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_sharpe_ratio(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_sharpe_ratio.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_overall_volatility(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_volatility(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_volatility.__name__),
-            show_legend=show_legend,
-        )
+def plot_rolling_smoothed_skewness(
+    returns_df: DataFrameFloat, length: int, show_legend: bool = True
+) -> go.Figure:
+    return widgets.curves(
+        returns_df=stats.get_rolling_smoothed_skewness(
+            returns_df=returns_df, length=length
+        ),
+        title=format_plot_name(name=plot_rolling_smoothed_skewness.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_overall_average_drawdown(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_average_drawdown(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_average_drawdown.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_overall_average_correlation(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_average_correlation(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_average_correlation.__name__),
-            show_legend=show_legend,
-        )
+def plot_overall_returns(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_returns(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_returns.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_overall_monthly_skew(self, show_legend: bool = True) -> go.Figure:
-        return widgets.bars(
-            series=stats.get_overall_monthly_skew(
-                returns_df=self.data_dfs.sub_portfolio_ovrll
-            ),
-            title=format_plot_name(name=self.plot_overall_monthly_skew.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_stats_distribution_violin(
-        self, returns_limit: int, show_legend: bool = True
-    ) -> go.Figure:
-        return widgets.violin(
-            data=stats.get_stats_distribution_violin(
-                returns_df=self.data_dfs.sub_portfolio_ovrll,
-                returns_limit=returns_limit,
-            ),
-            title=format_plot_name(name=self.plot_stats_distribution_violin.__name__),
-            show_legend=show_legend,
-        )
+def plot_overall_sharpe_ratio(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_sharpe_ratio(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_sharpe_ratio.__name__),
+        show_legend=show_legend,
+    )
 
-    def plot_stats_distribution_histogram(
-        self, returns_limit: float, show_legend: bool = True
-    ) -> go.Figure:
-        return widgets.histogram(
-            data=stats.get_stats_distribution_histogram(
-                returns_df=self.data_dfs.sub_portfolio_ovrll,
-                returns_limit=returns_limit,
-            ),
-            title=format_plot_name(
-                name=self.plot_stats_distribution_histogram.__name__
-            ),
-            show_legend=show_legend,
-        )
 
-    def plot_correlation_heatmap(self, show_legend: bool = True) -> go.Figure:
-        filled_correlation_matrix, labels_list, corr_matrix_normalised = (
-            stats.get_correlation_heatmap(returns_df=self.data_dfs.sub_portfolio_ovrll)
-        )
+def plot_overall_volatility(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_volatility(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_volatility.__name__),
+        show_legend=show_legend,
+    )
 
-        return widgets.heatmap(
-            z_values=filled_correlation_matrix,
-            x_labels=labels_list,
-            y_labels=labels_list,
-            z_normalized=corr_matrix_normalised,
-            title=format_plot_name(name=self.plot_correlation_heatmap.__name__),
-            show_legend=show_legend,
-        )
 
-    def plot_correlation_clusters_icicle(
-        self, max_clusters: int, show_legend: bool = True
-    ) -> go.Figure:
-        labels, parents = stats.get_correlation_clusters_icicle(
-            returns_df=self.data_dfs.sub_portfolio_ovrll, max_clusters=max_clusters
-        )
+def plot_overall_average_drawdown(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_average_drawdown(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_average_drawdown.__name__),
+        show_legend=show_legend,
+    )
 
-        return widgets.icicle(
-            labels=labels,
-            parents=parents,
-            title=format_plot_name(name=self.plot_correlation_clusters_icicle.__name__),
-            show_legend=show_legend,
-        )
+
+def plot_overall_average_correlation(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_average_correlation(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_average_correlation.__name__),
+        show_legend=show_legend,
+    )
+
+
+def plot_overall_monthly_skew(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    return widgets.bars(
+        series=stats.get_overall_monthly_skew(returns_df=returns_df),
+        title=format_plot_name(name=plot_overall_monthly_skew.__name__),
+        show_legend=show_legend,
+    )
+
+
+def plot_stats_distribution_violin(
+    returns_df: DataFrameFloat, returns_limit: int, show_legend: bool = True
+) -> go.Figure:
+    return widgets.violin(
+        data=stats.get_stats_distribution_violin(
+            returns_df=returns_df,
+            returns_limit=returns_limit,
+        ),
+        title=format_plot_name(name=plot_stats_distribution_violin.__name__),
+        show_legend=show_legend,
+    )
+
+
+def plot_stats_distribution_histogram(
+    returns_df: DataFrameFloat, returns_limit: float, show_legend: bool = True
+) -> go.Figure:
+    return widgets.histogram(
+        data=stats.get_stats_distribution_histogram(
+            returns_df=returns_df,
+            returns_limit=returns_limit,
+        ),
+        title=format_plot_name(name=plot_stats_distribution_histogram.__name__),
+        show_legend=show_legend,
+    )
+
+
+def plot_correlation_heatmap(
+    returns_df: DataFrameFloat, show_legend: bool = True
+) -> go.Figure:
+    filled_correlation_matrix, labels_list, corr_matrix_normalised = (
+        stats.get_correlation_heatmap(returns_df=returns_df)
+    )
+
+    return widgets.heatmap(
+        z_values=filled_correlation_matrix,
+        x_labels=labels_list,
+        y_labels=labels_list,
+        z_normalized=corr_matrix_normalised,
+        title=format_plot_name(name=plot_correlation_heatmap.__name__),
+        show_legend=show_legend,
+    )
+
+
+def plot_correlation_clusters_icicle(
+    returns_df: DataFrameFloat, max_clusters: int, show_legend: bool = True
+) -> go.Figure:
+    labels, parents = stats.get_correlation_clusters_icicle(
+        returns_df=returns_df, max_clusters=max_clusters
+    )
+
+    return widgets.icicle(
+        labels=labels,
+        parents=parents,
+        title=format_plot_name(name=plot_correlation_clusters_icicle.__name__),
+        show_legend=show_legend,
+    )
