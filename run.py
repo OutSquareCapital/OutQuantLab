daf43385a5c1a1f915178cff7460a8acf8be0d93
OutSquareCapital <outsquarecapital@gmail.com>
2025-01-29 17:19:06 +0100

    changes to main API as a results of precedents commits: returning data outside of the class was an error, I want a self-contained API for oql. hence, graphs package is now directly imported as an attribute of OQL class. It allows to get all the actions from the original package, whilst making them pure functions, AND keeping the same API. only diff is the fact that you have to put returns_df as an arg, but this is the flexibility price

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 def launch_app() -> None:
     oql: OutQuantLab = OutQuantLab()
     oql.run()
+    oql.graphs.plot_rolling_sharpe_ratio(returns_df=oql.data["lvl0"], length=252).show()  # type: ignore
 
 
 if __name__ == "__main__":
