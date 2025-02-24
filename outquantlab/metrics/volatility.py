425025bdd77d0c7201dd5c21eeef43cb49b3a936
OutSquareCapital <outsquarecapital@gmail.com>
2025-02-25 00:18:03 +0100

    Refactor overall volatility calculations to introduce get_lt_vol and get_composite_vol_raw functions; integrate calculate_overall_mean for filling missing values.

--- outquantlab/metrics/volatility.py
+++ outquantlab/metrics/volatility.py
@@ -1,8 +1,8 @@
 import bottleneck as bn  # type: ignore
 from numpy import isnan, nan, where
-
 from outquantlab.metrics.maths_constants import ANNUALIZED_PERCENTAGE_FACTOR
 from outquantlab.typing_conventions import ArrayFloat
+from outquantlab.metrics.aggregation import calculate_overall_mean
 
 
 def overall_volatility(returns_array: ArrayFloat) -> ArrayFloat:
@@ -27,22 +27,43 @@ def hv_composite(
     long_term_lengths: int = 1024,
     st_weight: float = 0.6,
 ) -> ArrayFloat:
+    st_vol: ArrayFloat = rolling_volatility(
+        array=returns_array, length=short_term_lengths, min_length=short_term_lengths
+    )
+    lt_vol: ArrayFloat = get_lt_vol(
+        returns_array=returns_array, long_term_lengths=long_term_lengths
+    )
+    composite_vol: ArrayFloat = get_composite_vol_raw(
+        st_weight=st_weight,
+        short_term_vol=st_vol,
+        long_term_vol=lt_vol,
+    )
+    return (
+        get_composite_vol_filled(composite_vol=composite_vol)
+        * ANNUALIZED_PERCENTAGE_FACTOR
+    )
+
+
+def get_lt_vol(returns_array: ArrayFloat, long_term_lengths: int) -> ArrayFloat:
     max_length: int = returns_array.shape[0]
     adjusted_length: int = (
         long_term_lengths if long_term_lengths < max_length else max_length
     )
 
-    short_term_vol_array: ArrayFloat = rolling_volatility(
-        array=returns_array, length=short_term_lengths
-    )
-    long_term_vol_array: ArrayFloat = rolling_volatility(
-        array=returns_array, length=adjusted_length
-    )
-    lt_weight: float = 1 - st_weight
+    return rolling_volatility(array=returns_array, length=adjusted_length, min_length=1)
 
-    return (
-        (st_weight * short_term_vol_array) + (lt_weight * long_term_vol_array)
-    ) * ANNUALIZED_PERCENTAGE_FACTOR
+
+def get_composite_vol_raw(
+    st_weight: float, short_term_vol: ArrayFloat, long_term_vol: ArrayFloat
+) -> ArrayFloat:
+    weighted_long_term_vol: ArrayFloat = long_term_vol * (1 - st_weight)
+    weighted_short_term_vol: ArrayFloat = short_term_vol * st_weight
+    return weighted_long_term_vol + weighted_short_term_vol
+
+
+def get_composite_vol_filled(composite_vol: ArrayFloat) -> ArrayFloat:
+    mean_vol: ArrayFloat = calculate_overall_mean(array=composite_vol, axis=0)
+    return where(isnan(composite_vol), mean_vol, composite_vol)
 
 
 def separate_volatility(
