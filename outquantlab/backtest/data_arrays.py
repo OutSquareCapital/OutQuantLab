312f2b1910e7817bfba0dddc0fc6a084cc76b3ea
OutSquareCapital <outsquarecapital@gmail.com>
2025-02-24 16:50:06 +0100

    Refactor DataArrays class to use NamedTuple and add create_data_arrays for init

--- outquantlab/backtest/data_arrays.py
+++ outquantlab/backtest/data_arrays.py
@@ -1,39 +1,47 @@
-from outquantlab.metrics import (
-    calculate_equity_curves,
-    hv_composite,
-    log_returns_np
-)
+from outquantlab.metrics import calculate_equity_curves, hv_composite, log_returns_np
 from outquantlab.typing_conventions import ArrayFloat
 from numpy import empty_like, nan
+from typing import NamedTuple
 
-class DataArrays:
-    def __init__(self, returns_array: ArrayFloat) -> None:
-        self.prices_array: ArrayFloat = calculate_equity_curves(
-            returns_array=returns_array
-        )
 
-        self.hv_array: ArrayFloat = hv_composite(returns_array=returns_array)
+class DataArrays(NamedTuple):
+    prices_array: ArrayFloat
+    hv_array: ArrayFloat
+    log_returns_array: ArrayFloat
+    adjusted_returns_array: ArrayFloat
 
-        self.log_returns_array: ArrayFloat = shift_array(
-            original_array=log_returns_np(prices_array=self.prices_array)
-        )
-        self.prices_array: ArrayFloat = shift_array(original_array=self.prices_array)
-        self.adjusted_returns_array: ArrayFloat = calculate_volatility_adjusted_returns(
-            pct_returns_array=returns_array, hv_array=self.hv_array
-        )
 
 def calculate_volatility_adjusted_returns(
     pct_returns_array: ArrayFloat, hv_array: ArrayFloat, target_volatility: int = 15
 ) -> ArrayFloat:
     vol_adj_position_size = target_volatility / hv_array
-    
+
     vol_adj_position_size_shifted: ArrayFloat = shift_array(
         original_array=vol_adj_position_size
     )
     return pct_returns_array * vol_adj_position_size_shifted
+
 
 def shift_array(original_array: ArrayFloat, step: int = 1) -> ArrayFloat:
     shifted_array: ArrayFloat = empty_like(prototype=original_array)
     shifted_array[step:, :] = original_array[:-step, :]
     shifted_array[:step, :] = nan
     return shifted_array
+
+
+def create_data_arrays(returns_array: ArrayFloat) -> DataArrays:
+    prices: ArrayFloat = calculate_equity_curves(returns_array=returns_array)
+    hv: ArrayFloat = hv_composite(returns_array=returns_array)
+    log_returns: ArrayFloat = shift_array(
+        original_array=log_returns_np(prices_array=prices)
+    )
+    prices_shifted: ArrayFloat = shift_array(original_array=prices)
+    adjusted_returns: ArrayFloat = calculate_volatility_adjusted_returns(
+        pct_returns_array=returns_array, hv_array=hv
+    )
+    return DataArrays(
+        prices_array=prices_shifted,
+        hv_array=hv,
+        log_returns_array=log_returns,
+        adjusted_returns_array=adjusted_returns,
+    )
