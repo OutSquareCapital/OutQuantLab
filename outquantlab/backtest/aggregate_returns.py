f6b6bf00ee62b6ab159a640bf3e109607cfa59f7
OutSquareCapital <outsquarecapital@gmail.com>
2025-02-24 16:49:45 +0100

    Add aggregate_raw_returns function and refactor execute_backtest to use create_data_arrays

--- outquantlab/backtest/aggregate_returns.py
+++ outquantlab/backtest/aggregate_returns.py
@@ -20,3 +20,20 @@ def get_global_portfolio_returns(returns_df: DataFrameFloat) -> DataFrameFloat:
         index=returns_df.dates,
         columns=["Portfolio"],
     )
+
+def aggregate_raw_returns(returns_df: DataFrameFloat) -> dict[str, DataFrameFloat]:
+    portfolio_dict: dict[str, DataFrameFloat] = {}
+    clusters_depth: int = len(returns_df.columns.names)
+    for lvl in range(clusters_depth, 0, -1):
+        returns_df = calculate_portfolio_returns(
+            returns_df=returns_df,
+            grouping_levels=returns_df.columns.names[:lvl],
+        )
+
+        returns_df.dropna(axis=0, how="all", inplace=True)  # type: ignore
+        key_name: str = returns_df.columns.names[lvl - 1]
+        portfolio_dict[key_name] = returns_df
+
+    portfolio_dict["lvl0"] = get_global_portfolio_returns(returns_df=returns_df)
+
+    return portfolio_dict
--- outquantlab/backtest/execute_backtest.py
+++ outquantlab/backtest/execute_backtest.py
@@ -1,11 +1,9 @@
-from outquantlab.backtest.aggregate_returns import (
-    calculate_portfolio_returns,
-    get_global_portfolio_returns,
-)
+from outquantlab.backtest.aggregate_returns import aggregate_raw_returns
+from outquantlab.backtest.data_arrays import create_data_arrays
 from outquantlab.backtest.process_strategies import process_strategies
 from outquantlab.config_classes import BacktestConfig
 from outquantlab.typing_conventions import ArrayFloat, DataFrameFloat
-from outquantlab.backtest.data_arrays import DataArrays
+
 
 def execute_backtest(
     returns_df: DataFrameFloat, backtest_config: BacktestConfig
@@ -22,7 +20,7 @@ def get_strategies_returns(
     backtest_config: BacktestConfig,
 ) -> DataFrameFloat:
     results_array: ArrayFloat = process_strategies(
-        data_arrays=DataArrays(returns_array=returns_df.get_array()),
+        data_arrays=create_data_arrays(returns_array=returns_df.get_array()),
         backtest_config=backtest_config,
     )
     return DataFrameFloat(
@@ -30,21 +28,3 @@ def get_strategies_returns(
         index=returns_df.dates,
         columns=backtest_config.multi_index,
     )
-
-
-def aggregate_raw_returns(returns_df: DataFrameFloat) -> dict[str, DataFrameFloat]:
-    portfolio_dict: dict[str, DataFrameFloat] = {}
-    clusters_depth: int = len(returns_df.columns.names)
-    for lvl in range(clusters_depth, 0, -1):
-        returns_df = calculate_portfolio_returns(
-            returns_df=returns_df,
-            grouping_levels=returns_df.columns.names[:lvl],
-        )
-
-        returns_df.dropna(axis=0, how="all", inplace=True)  # type: ignore
-        key_name: str = returns_df.columns.names[lvl - 1]
-        portfolio_dict[key_name] = returns_df
-
-    portfolio_dict["lvl0"] = get_global_portfolio_returns(returns_df=returns_df)
-
-    return portfolio_dict
