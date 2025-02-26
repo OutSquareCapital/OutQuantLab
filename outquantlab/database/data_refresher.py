d375ea9fc0af5f9befcd0644f330cd4c296dae0b
OutSquareCapital <outsquarecapital@gmail.com>
2025-02-26 19:14:55 +0100

    Refactor data refresh logic by extracting price and return data retrieval into separate functions

--- outquantlab/database/data_refresher.py
+++ outquantlab/database/data_refresher.py
@@ -4,9 +4,14 @@ from outquantlab.typing_conventions import DataFrameFloat
 from pandas import DataFrame
 import yfinance as yf  # type: ignore
 
-
-    
 def refresh_yf_data(dbq: DataQueries, assets: list[str]) -> None:
+    prices_data: DataFrameFloat = _get_prices_data(assets=assets)
+    returns_data: DataFrameFloat = _get_returns_data(prices_data=prices_data)
+    _save_data(
+        dbq=dbq, prices_data=prices_data, returns_data=returns_data
+    )
+
+def _get_prices_data(assets: list[str]) -> DataFrameFloat:
     data: DataFrame | None = yf.download(  # type: ignore
         tickers=assets,
         interval="1d",
@@ -16,16 +21,22 @@ def refresh_yf_data(dbq: DataQueries, assets: list[str]) -> None:
     if data is None:
         raise ValueError("Yahoo Finance Data Not Available")
     else:
-        prices_data = DataFrameFloat(data=data["Close"])  # type: ignore
+        return DataFrameFloat(data=data["Close"])  # type: ignore
 
-        returns_data = DataFrameFloat(
-            data=pct_returns_np(prices_array=prices_data.get_array()),
-            columns=prices_data.columns,
-            index=prices_data.dates,
-        )
-        
-        assets_names: list[str] = prices_data.columns.to_list()
 
-        dbq.select(file="price_data").save(data=prices_data)
-        dbq.select(file="returns_data").save(data=returns_data)
-        dbq.select(file="assets_names").save(data=assets_names)
+def _get_returns_data(prices_data: DataFrameFloat) -> DataFrameFloat:
+    return DataFrameFloat(
+        data=pct_returns_np(prices_array=prices_data.get_array()),
+        columns=prices_data.columns,
+        index=prices_data.dates,
+    )
+
+
+def _save_data(
+    dbq: DataQueries, prices_data: DataFrameFloat, returns_data: DataFrameFloat
+) -> None:
+    assets_names: list[str] = prices_data.columns.to_list()
+
+    dbq.select(file="price_data").save(data=prices_data)
+    dbq.select(file="returns_data").save(data=returns_data)
+    dbq.select(file="assets_names").save(data=assets_names)
