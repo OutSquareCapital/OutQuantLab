8a58b43e8e568176f046240ba272cd46ed63c6b1
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-05 02:27:24 +0100

    minor code organization changes

--- outquantlab/backtest/data_arrays.py
+++ outquantlab/backtest/data_arrays.py
@@ -8,36 +8,31 @@ from outquantlab.typing_conventions import ArrayFloat
 
 class DataArrays(NamedTuple):
     prices: ArrayFloat
-    hv: ArrayFloat
     log_returns: ArrayFloat
     adjusted_returns: ArrayFloat
+    hv: ArrayFloat
 
 
 def create_data_arrays(returns_array: ArrayFloat) -> DataArrays:
     prices: ArrayFloat = calculate_equity_curves(returns_array=returns_array)
+    returns: ArrayFloat = log_returns_np(prices_array=prices)
     hv: ArrayFloat = hv_composite(returns_array=returns_array)
-    log_returns: ArrayFloat = _shift_array(
-        original_array=log_returns_np(prices_array=prices)
-    )
-    prices_shifted: ArrayFloat = _shift_array(original_array=prices)
     adjusted_returns: ArrayFloat = calculate_volatility_adjusted_returns(
         pct_returns_array=returns_array, hv_array=hv
     )
     return DataArrays(
-        prices=prices_shifted,
-        hv=hv,
-        log_returns=log_returns,
+        prices=_shift_array(original_array=prices),
+        log_returns=_shift_array(original_array=returns),
         adjusted_returns=adjusted_returns,
+        hv=hv,
     )
 
 
 def calculate_volatility_adjusted_returns(
     pct_returns_array: ArrayFloat, hv_array: ArrayFloat, target_volatility: int = 25
 ) -> ArrayFloat:
-    vol_adj_position_size = target_volatility / hv_array
-
     vol_adj_position_size_shifted: ArrayFloat = _shift_array(
-        original_array=vol_adj_position_size
+        original_array=target_volatility / hv_array
     )
     return pct_returns_array * vol_adj_position_size_shifted
 
