e204640327f75e4eff28e51e949d0d36b5d08c0f
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-10 14:59:49 +0100

    file renaming and MetricsFuncs enum introduced for get metrics func

---
+++ outquantlab/stats/stats_interfaces.py
@@ -0,0 +1,39 @@
+from collections.abc import Callable
+from outquantlab.typing_conventions import DataFrameFloat, SeriesFloat, ArrayFloat
+from typing import TypeAlias
+import outquantlab.metrics as mt
+from enum import Enum
+
+RollingMetricFunc: TypeAlias = Callable[[ArrayFloat, int], ArrayFloat]
+OverallMetricFunc: TypeAlias = Callable[[ArrayFloat], ArrayFloat]
+
+
+class MetricFuncs(Enum):
+    total_returns: OverallMetricFunc = mt.calculate_total_returns
+    sharpe_ratio: OverallMetricFunc = mt.overall_sharpe_ratio
+    max_drawdown: OverallMetricFunc = mt.calculate_max_drawdown
+    volatility: OverallMetricFunc = mt.overall_volatility_annualized
+
+
+def get_df_stats_interface(
+    returns_df: DataFrameFloat,
+    stats_func: RollingMetricFunc,
+    ascending: bool,
+    length: int,
+) -> DataFrameFloat:
+    return DataFrameFloat(
+        data=stats_func(returns_df.get_array(), length),
+        index=returns_df.dates,
+        columns=returns_df.get_names(),
+    ).sort_data(ascending=ascending)
+
+
+def get_series_stats_interface(
+    returns_df: DataFrameFloat,
+    stats_func: OverallMetricFunc,
+    ascending: bool,
+) -> SeriesFloat:
+    return SeriesFloat(
+        data=stats_func(returns_df.get_array()),
+        index=returns_df.get_names(),
+    ).sort_data(ascending=ascending)
