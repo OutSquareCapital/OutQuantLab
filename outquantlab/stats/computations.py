6e96495d907cbb7cca76ca7c1b5a9d085093cd71
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-10 16:05:45 +0100

    renaming of metric/stats funcs as described earlier. also, get_metrics func refactored to account for new Enum which directly handle the called metrics funcs.

--- outquantlab/stats/computations.py
+++ outquantlab/stats/computations.py
@@ -1,20 +1,14 @@
 import outquantlab.metrics as mt
-from outquantlab.stats.interfaces import (
+from outquantlab.stats.stats_interfaces import (
     get_df_stats_interface,
     get_series_stats_interface,
 )
-from outquantlab.typing_conventions import (
-    ArrayFloat,
-    DataFrameFloat,
-    SeriesFloat,
-    OverallStatFunc,
-)
+from outquantlab.typing_conventions import DataFrameFloat, SeriesFloat, ArrayFloat
+from outquantlab.stats.stats_interfaces import MetricFuncs
 
 
 def _format_metric_name(name: str) -> str:
-    return (
-        name.replace("calculate_", "").replace("overall_", "").replace("_", " ").title()
-    )
+    return name.replace("_", " ").title()
 
 
 def get_metrics(returns_df: DataFrameFloat) -> dict[str, float]:
@@ -23,140 +17,12 @@ def get_metrics(returns_df: DataFrameFloat) -> dict[str, float]:
         print("Metrics can only be calculated for a single asset")
         return {}
     else:
-        metric_functions: list[OverallStatFunc] = [
-            mt.calculate_total_returns,
-            mt.overall_sharpe_ratio,
-            mt.calculate_max_drawdown,
-            mt.overall_volatility_annualized,
-        ]
-
-        metric_names: list[str] = [
-            _format_metric_name(name=func.__name__) for func in metric_functions
-        ]
-        results: list[ArrayFloat] = [func(array) for func in metric_functions]
-
+        results: list[ArrayFloat] = [func.value(array) for func in MetricFuncs]
+        names: list[str] = [_format_metric_name(name=func.name) for func in MetricFuncs]
         return {
             name: round(number=result.item(), ndigits=2)
-            for name, result in zip(metric_names, results)
+            for name, result in zip(names, results)
         }
-
-
-def get_stats_equity(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_equity_curves,
-        ascending=True,
-        length=length,
-    )
-
-
-def get_rolling_volatility(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.rolling_volatility,
-        ascending=False,
-        length=length,
-    )
-
-
-def get_rolling_drawdown(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_rolling_drawdown,
-        ascending=False,
-        length=length,
-    )
-
-
-def get_rolling_sharpe_ratio(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.rolling_sharpe_ratio,
-        ascending=False,
-        length=length,
-    )
-
-
-def get_rolling_smoothed_skewness(
-    returns_df: DataFrameFloat, length: int
-) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.rolling_skewness,
-        ascending=True,
-        length=length,
-    )
-
-
-def get_overall_returns(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_total_returns,
-        ascending=True,
-    )
-
-
-def get_overall_sharpe_ratio(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.overall_sharpe_ratio,
-        ascending=False,
-    )
-
-
-def get_overall_volatility(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.overall_volatility_annualized,
-        ascending=True,
-    )
-
-
-def get_overall_average_drawdown(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_overall_average_drawdown,
-        ascending=True,
-    )
-
-
-def get_overall_average_correlation(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_overall_average_correlation,
-        ascending=True,
-    )
-
-
-def get_overall_monthly_skew(returns_df: DataFrameFloat) -> SeriesFloat:
-    return get_series_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.calculate_overall_monthly_skewness,
-        ascending=True,
-    )
-
-
-def get_stats_distribution_violin(
-    returns_df: DataFrameFloat, returns_limit: int
-) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.limit_outliers,
-        ascending=True,
-        length=returns_limit,
-    )
-
-
-def get_stats_distribution_histogram(
-    returns_df: DataFrameFloat, returns_limit: int
-) -> DataFrameFloat:
-    return get_df_stats_interface(
-        returns_df=returns_df,
-        stats_func=mt.limit_outliers,
-        ascending=True,
-        length=returns_limit,
-    )
-
 
 def get_correlation_heatmap(
     returns_df: DataFrameFloat,
@@ -174,4 +40,120 @@ def get_correlation_clusters_icicle(
         returns_array=returns_df.get_array(),
         asset_names=returns_df.get_names(),
         max_clusters=max_clusters,
+    )
+
+def get_stats_equity(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_equity_curves,
+        ascending=True,
+        length=length,
+    )
+
+
+def get_rolling_volatility(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.rolling_volatility,
+        ascending=False,
+        length=length,
+    )
+
+
+def get_rolling_drawdown(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_rolling_drawdown,
+        ascending=False,
+        length=length,
+    )
+
+
+def get_rolling_sharpe_ratio(returns_df: DataFrameFloat, length: int) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.rolling_sharpe_ratio,
+        ascending=False,
+        length=length,
+    )
+
+
+def get_rolling_smoothed_skewness(
+    returns_df: DataFrameFloat, length: int
+) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.rolling_skewness,
+        ascending=True,
+        length=length,
+    )
+
+
+def get_overall_returns(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_total_returns,
+        ascending=True,
+    )
+
+
+def get_overall_sharpe_ratio(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.overall_sharpe_ratio,
+        ascending=False,
+    )
+
+
+def get_overall_volatility(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.overall_volatility_annualized,
+        ascending=True,
+    )
+
+
+def get_overall_average_drawdown(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_overall_average_drawdown,
+        ascending=True,
+    )
+
+
+def get_overall_average_correlation(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_overall_average_correlation,
+        ascending=True,
+    )
+
+
+def get_overall_monthly_skew(returns_df: DataFrameFloat) -> SeriesFloat:
+    return get_series_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.calculate_overall_monthly_skewness,
+        ascending=True,
+    )
+
+
+def get_stats_distribution_violin(
+    returns_df: DataFrameFloat, returns_limit: int
+) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.limit_outliers,
+        ascending=True,
+        length=returns_limit,
+    )
+
+
+def get_stats_distribution_histogram(
+    returns_df: DataFrameFloat, returns_limit: int
+) -> DataFrameFloat:
+    return get_df_stats_interface(
+        returns_df=returns_df,
+        metric_func=mt.limit_outliers,
+        ascending=True,
+        length=returns_limit,
     )
