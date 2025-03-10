49be93e3b08d0ea1326953b795a7a53047aa6ddd
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-10 16:07:41 +0100

    minor changes

--- outquantlab/typing_conventions/__init__.py
+++ outquantlab/typing_conventions/__init__.py
@@ -1,26 +1,17 @@
-
-from outquantlab.typing_conventions.custom_classes import (
-    SeriesFloat,
-    DataFrameFloat
-)
+from outquantlab.typing_conventions.custom_classes import SeriesFloat, DataFrameFloat
 
 from outquantlab.typing_conventions.custom_types import (
     Float32,
     Int32,
     ArrayFloat,
     ArrayInt,
-    OverallStatFunc,
-    RollingStatFunc
 )
 
 __all__: list[str] = [
-    # Types
-    'Float32',
-    'Int32',
-    'ArrayFloat',
-    'ArrayInt',
-    'SeriesFloat',
-    'DataFrameFloat',
-    'OverallStatFunc',
-    'RollingStatFunc'
+    "Float32",
+    "Int32",
+    "ArrayFloat",
+    "ArrayInt",
+    "SeriesFloat",
+    "DataFrameFloat",
 ]
