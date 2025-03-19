f163c01e2b254ffb0a65cd9fa5529286546365c3
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-03-19 13:46:42 +0100

    refactored plot_class to take into account new stats provider naming, and direct call to ".show" method inside of it, since it's now clearly made for local usage

--- outquantlab/local_ploty/plot_class.py
+++ outquantlab/local_ploty/plot_class.py
@@ -1,8 +1,7 @@
-from outquantlab.local_ploty.graph_class import Graph
 from outquantlab.stats import (
-    StatsDF,
+    StatsCurves,
     StatsOverall,
-    StatsSeries,
+    StatsBars,
     StatsDistribution,
 )
 from outquantlab.local_ploty.widgets import (
@@ -16,8 +15,8 @@ from outquantlab.typing_conventions import DataFrameFloat
 from collections.abc import Callable
 from typing import TypeAlias
 
-DFMethod: TypeAlias = Callable[[DataFrameFloat, int], StatsDF]
-SeriesMethod: TypeAlias = Callable[[DataFrameFloat], StatsSeries]
+CurvesMethod: TypeAlias = Callable[[DataFrameFloat, int], StatsCurves]
+BarsMethod: TypeAlias = Callable[[DataFrameFloat], StatsBars]
 OverallMethod: TypeAlias = Callable[[DataFrameFloat], StatsOverall]
 DistributionMethod: TypeAlias = Callable[[DataFrameFloat, int], StatsDistribution]
 
@@ -31,37 +30,37 @@ class Plots:
         self.histogram = Histogram()
 
     def plot_curves(
-        self, returns_df: DataFrameFloat, length: int, stats_method: DFMethod
-    ) -> Graph:
-        processor: StatsDF = stats_method(returns_df, length)
-        return self.curves.get_fig(data=processor.data, title=processor.title)
+        self, returns_df: DataFrameFloat, length: int, stats_method: CurvesMethod
+    ) -> None:
+        processor: StatsCurves = stats_method(returns_df, length)
+        return self.curves.get_fig(data=processor.data, title=processor.title).show()
 
     def plot_bars(
-        self, returns_df: DataFrameFloat, stats_method: SeriesMethod
-    ) -> Graph:
-        processor: StatsSeries = stats_method(returns_df)
-        return self.bars.get_fig(data=processor.data, title=processor.title)
+        self, returns_df: DataFrameFloat, stats_method: BarsMethod
+    ) -> None:
+        processor: StatsBars = stats_method(returns_df)
+        return self.bars.get_fig(data=processor.data, title=processor.title).show()
 
     def plot_table(
         self, returns_df: DataFrameFloat, stats_method: OverallMethod
-    ) -> Graph:
+    ) -> None:
         processor: StatsOverall = stats_method(returns_df)
-        return self.table.get_fig(data=processor.data, title=processor.title)
+        return self.table.get_fig(data=processor.data, title=processor.title).show()
 
     def plot_violins(
         self,
         returns_df: DataFrameFloat,
-        returns_limit: int,
         stats_method: DistributionMethod,
-    ) -> Graph:
-        processor: StatsDistribution = stats_method(returns_df, returns_limit)
-        return self.violins.get_fig(data=processor.data, title=processor.title)
+        frequency: int,
+    ) -> None:
+        processor: StatsDistribution = stats_method(returns_df, frequency)
+        return self.violins.get_fig(data=processor.data, title=processor.title).show()
 
     def plot_histogram(
         self,
         returns_df: DataFrameFloat,
-        returns_limit: int,
         stats_method: DistributionMethod,
-    ) -> Graph:
-        processor: StatsDistribution = stats_method(returns_df, returns_limit)
-        return self.histogram.get_fig(data=processor.data, title=processor.title)
+        frequency: int,
+    ) -> None:
+        processor: StatsDistribution = stats_method(returns_df, frequency)
+        return self.histogram.get_fig(data=processor.data, title=processor.title).show()
