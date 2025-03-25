dfff46041c8ccf342cc8d194b9244d4ed08d7246
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-03-25 16:36:39 +0100

    moved data arrays in indicators package. updated processor for better decoupling, and to avoid passsing the returns df where it's not necessary

--- outquantlab/backtest/processor.py
+++ outquantlab/backtest/processor.py
@@ -1,23 +1,27 @@
-from outquantlab.metrics import rolling_scalar_normalisation
-from outquantlab.typing_conventions import DataFrameFloat, ArrayFloat, Float32
-from numpy import empty
-from outquantlab.indicators import BaseIndic
 from concurrent.futures import ThreadPoolExecutor
 from os import cpu_count
-from outquantlab.backtest.data_arrays import DataArrays
+
+from numpy import empty
+
 from outquantlab.config_classes import BacktestConfig, BacktestResults
+from outquantlab.indicators import BaseIndic, DataArrays
+from outquantlab.typing_conventions import ArrayFloat, DataFrameFloat, Float32
+
 
 def process_backtest(
     returns_df: DataFrameFloat,
     config: BacktestConfig,
 ) -> BacktestResults:
     processor: IndicatorsProcessor = IndicatorsProcessor(
-        indics_params=config.indics_params,
-        returns_df=returns_df,
-        total_returns_streams=len(config.multi_index)
+        assets_nb=returns_df.shape[1],
+        days_nb=returns_df.shape[0],
+        total_returns_streams=len(config.multi_index),
     )
     returns_df = DataFrameFloat(
-        data=processor.process_strategies(),
+        data=processor.process_strategies(
+            indics_params=config.indics_params,
+            data_arrays=DataArrays(pct_returns=returns_df.get_array()),
+        ),
         index=returns_df.dates,
         columns=config.multi_index,
     )
@@ -28,16 +32,14 @@ def process_backtest(
 class IndicatorsProcessor:
     def __init__(
         self,
-        indics_params: list[BaseIndic],
-        returns_df: DataFrameFloat,
+        assets_nb: int,
+        days_nb: int,
         total_returns_streams: int,
     ) -> None:
-        self.indics_params: list[BaseIndic] = indics_params
-        self.data_arrays: DataArrays = DataArrays(pct_returns=returns_df.get_array())
-        self.assets_nb: int = returns_df.shape[1]
+        self.assets_nb: int = assets_nb
         self.start_index: int = 0
-        self.results: ArrayFloat =empty(
-            shape=(returns_df.shape[0], total_returns_streams),
+        self.results: ArrayFloat = empty(
+            shape=(days_nb, total_returns_streams),
             dtype=Float32,
         )
 
@@ -52,43 +54,23 @@ class IndicatorsProcessor:
             self.start_index = end_index
 
     def process_strategies(
-        self,
+        self, indics_params: list[BaseIndic], data_arrays: DataArrays
     ) -> ArrayFloat:
         threads_nb: int = cpu_count() or 8
         with ThreadPoolExecutor(max_workers=threads_nb) as global_executor:
-            for indic in self.indics_params:
+            for indic in indics_params:
                 try:
-                    results_list: list[ArrayFloat] = self._process_params_parallel(
-                        indic=indic,
+                    results_list: list[ArrayFloat] = indic.process_params_parallel(
+                        data_arrays=data_arrays,
                         global_executor=global_executor,
                     )
 
                     self.fill_results_array(
                         results_list=results_list,
-                        params_nb=indic.params_nb,
+                        params_nb=indic.params.quantity,
                     )
 
                 except Exception as e:
-                    raise Exception(f"Error processing indicator {indic.name}: {e}")
+                    raise Exception(f"Error processing indicator {indic}: {e}")
 
         return self.results
-
-    def _process_params_parallel(
-        self,
-        indic: BaseIndic,
-        global_executor: ThreadPoolExecutor,
-    ) -> list[ArrayFloat]:
-        def process_single_param(param_tuple: tuple[int, ...]) -> ArrayFloat:
-            return self._process_param(indic=indic, param_tuple=param_tuple)
-
-        return list(global_executor.map(process_single_param, indic.param_combos))
-
-    def _process_param(
-        self, indic: BaseIndic, param_tuple: tuple[int, ...]
-    ) -> ArrayFloat:
-        return (
-            rolling_scalar_normalisation(
-                raw_signal=indic.execute(self.data_arrays, *param_tuple)
-            )
-            * self.data_arrays.adjusted_returns
-        )
