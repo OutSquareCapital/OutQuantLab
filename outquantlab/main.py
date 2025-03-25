bda821f927af8a9780529cca9d9292b4a340a3ef
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-03-25 03:00:37 +0100

    various refactors of the main files in consequence of the previous commits

--- outquantlab/main.py
+++ outquantlab/main.py
@@ -1,7 +1,8 @@
-from outquantlab.backtest import BacktestResults, execute_backtest
-from outquantlab.config_classes import AppConfig
+from outquantlab.config_classes import AppConfig, BacktestResults
+from outquantlab.backtest import process_backtest
 from outquantlab.database import DataBaseProvider
-from outquantlab.local_ploty import Plots
+
+# from outquantlab.local_ploty import Plots
 from outquantlab.stats import StatsProvider
 
 
@@ -10,31 +11,30 @@ class OutQuantLab:
         self._dbp = DataBaseProvider()
         self.app_config: AppConfig = self._dbp.get_app_config()
         self.stats = StatsProvider()
-        if local:
-            self.plots = Plots()
 
     def run(self) -> BacktestResults:
-        return execute_backtest(
+        return process_backtest(
             returns_df=self._dbp.get_returns_data(
                 names=self.app_config.assets_config.get_all_active_entities_names()
             ),
-            backtest_config=self.app_config.get_backtest_config(),
+            config=self.app_config.get_backtest_config(),
         )
 
-    def save_results(self) -> None:
-        portfolio_curves: dict[str, dict[str, list[str]]] = (
-            self.stats.equity_curves.get_data(
-                returns_df=self.data["portfolio"],  # type: ignore
+    def save_results(self, results: BacktestResults) -> None:
+        results_dict: dict[str, dict[str, dict[str, list[str]]]] = {}
+        results_dict['rolling_sharpe_portfolio'] = (
+            self.stats.rolling_sharpe_ratio.get_data(
+                returns_df=results.portfolio,
                 length=2500,
-            ).convert_to_json(title=self.stats.equity_curves.title)
+            ).convert_to_json(title=self.stats.rolling_sharpe_ratio.title)
         )
-        assets_sharpes: dict[str, dict[str, list[str]]] = (
+        results_dict['overall_sharpe_assets'] = (
             self.stats.overall_sharpe.get_data(
-                returns_df=self.data["assets"]  # type: ignore
+                returns_df=results.assets
             ).convert_to_json(title=self.stats.overall_sharpe.title)
         )
 
-        self._dbp.save_backtest_results(results=[portfolio_curves, assets_sharpes])
+        self._dbp.save_backtest_results(results=results_dict)
 
     def save_config(self) -> None:
         self._dbp.save_app_config(config=self.app_config)
--- run.py
+++ run.py
@@ -1,23 +1,18 @@
-def launch_app() -> None:
-    print("running backtest...")
+def check_time() -> None:
+    import time
     start: float = time.perf_counter()
     oql = OutQuantLab()
     oql.run()
     end: float = time.perf_counter()
     print(f"Backtest completed in {end - start:.2f} seconds.")
-    oql.plots.plot_curves(
-        returns_df=oql.data["portfolio"], length=2500, metric=oql.stats.equity_curves # type: ignore
-    )
 
+def test_app() -> None:
+    oql = OutQuantLab(local=False)
+    results = oql.run()
+    print(results.portfolio)
 
 if __name__ == "__main__":
     print("initializing OutQuantLab...")
-
-    from sys import exit
-
     from outquantlab import OutQuantLab
-    import time
-
     print("OutQuantLab initialized")
-    launch_app()
-    exit(0)
+    test_app()
