e2ec01eba663ebcc659346bf89dc8f306a8d29cd
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-03-25 02:54:29 +0100

    refactored data arrays for a better API

--- outquantlab/backtest/data_arrays.py
+++ outquantlab/backtest/data_arrays.py
@@ -1,33 +1,28 @@
-from typing import NamedTuple
-
 from numpy import empty_like, nan
 
 from outquantlab.metrics import get_equity_curves, hv_composite, log_returns_np
 from outquantlab.typing_conventions import ArrayFloat
+from dataclasses import dataclass, field
 
 
-class DataArrays(NamedTuple):
-    prices: ArrayFloat
-    log_returns: ArrayFloat
-    adjusted_returns: ArrayFloat
-    hv: ArrayFloat
+@dataclass(slots=True)
+class DataArrays:
+    pct_returns: ArrayFloat
+    prices: ArrayFloat = field(init=False)
+    log_returns: ArrayFloat = field(init=False)
+    adjusted_returns: ArrayFloat = field(init=False)
+    hv: ArrayFloat = field(init=False)
 
+    def __post_init__(self) -> None:
+        prices: ArrayFloat = get_equity_curves(returns_array=self.pct_returns)
+        returns: ArrayFloat = log_returns_np(prices_array=prices)
+        self.hv: ArrayFloat = hv_composite(returns_array=self.pct_returns)
+        self.adjusted_returns: ArrayFloat = get_volatility_adjusted_returns(
+            pct_returns_array=self.pct_returns, hv_array=self.hv
+        )
 
-def create_data_arrays(returns_array: ArrayFloat) -> DataArrays:
-    prices: ArrayFloat = get_equity_curves(
-        returns_array=returns_array, length=returns_array.shape[0]
-    )
-    returns: ArrayFloat = log_returns_np(prices_array=prices)
-    hv: ArrayFloat = hv_composite(returns_array=returns_array)
-    adjusted_returns: ArrayFloat = get_volatility_adjusted_returns(
-        pct_returns_array=returns_array, hv_array=hv
-    )
-    return DataArrays(
-        prices=_shift_array(original_array=prices),
-        log_returns=_shift_array(original_array=returns),
-        adjusted_returns=adjusted_returns,
-        hv=hv,
-    )
+        self.prices = _shift_array(original_array=prices)
+        self.log_returns = _shift_array(original_array=returns)
 
 
 def get_volatility_adjusted_returns(
