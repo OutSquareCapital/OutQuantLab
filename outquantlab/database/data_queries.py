d54218938af25032f00ba047af79232c26c321fb
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-27 23:34:25 +0100

    changed data queries into a hierarchy of tuple for a better overview of the db structure, and a more convenient API. the code duplication still bothers me but it's non-urgent

--- outquantlab/database/data_queries.py
+++ outquantlab/database/data_queries.py
@@ -1,26 +1,64 @@
 from pathlib import Path
-
 from outquantlab.database.data_file import JSONFile, ParquetFile
+from typing import NamedTuple
+
+
+class AssetFiles(NamedTuple):
+    active: JSONFile[str, bool]
+    clusters: JSONFile[str, dict[str, list[str]]]
+
+
+class IndicFiles(NamedTuple):
+    active: JSONFile[str, bool]
+    params: JSONFile[str, dict[str, list[int]]]
+    clusters: JSONFile[str, dict[str, list[str]]]
+
+
+class BacktestFiles(NamedTuple):
+    results: JSONFile[str, dict[str, list[str]]]
+    returns: ParquetFile
+    prices: ParquetFile
+    
+class DataBase(NamedTuple):
+    assets: AssetFiles
+    indics: IndicFiles
+    backtest: BacktestFiles
 
 DB = "data"
 
-class DataQueries:
-    def __init__(self) -> None:
-        db_path: Path = self._get_db_path(db_name=DB)
-        self.assets_active: JSONFile = JSONFile(db_path=db_path, file_name="assets_active")
-        self.assets_clusters: JSONFile = JSONFile(db_path=db_path, file_name="assets_clusters")
-        self.indics_active: JSONFile = JSONFile(db_path=db_path, file_name="indics_active")
-        self.indics_params: JSONFile = JSONFile(db_path=db_path, file_name="indics_params")
-        self.indics_clusters: JSONFile = JSONFile(db_path=db_path, file_name="indics_clusters")
-        self.backtest_results: JSONFile = JSONFile(db_path=db_path, file_name="backtest_results")
-        self.returns_data: ParquetFile = ParquetFile(db_path=db_path, file_name="returns_data")
-        self.prices_data: ParquetFile = ParquetFile(db_path=db_path, file_name="prices_data")
 
-    def _get_db_path(self, db_name: str) -> Path:
-        current_file_path: Path = Path(__file__).resolve()
-        current_dir: Path = current_file_path.parent
-        return current_dir / db_name
+def get_db() -> DataBase:
+    db_path: Path = _get_db_path(db_name=DB)
+    return DataBase(
+        assets=_get_asset_files(db_path=db_path),
+        indics=_get_indic_files(db_path=db_path),
+        backtest=_get_backtest_files(db_path=db_path),
+    )
 
-    def check_data(self) -> None:
-        for name, value in self.__dict__.items():
-            print(f"{name}:\n {value}")
+def _get_db_path(db_name: str) -> Path:
+    current_file_path: Path = Path(__file__).resolve()
+    current_dir: Path = current_file_path.parent
+    return current_dir / db_name
+
+
+def _get_asset_files(db_path: Path) -> AssetFiles:
+    return AssetFiles(
+        active=JSONFile(db_path=db_path, file_name="assets_active"),
+        clusters=JSONFile(db_path=db_path, file_name="assets_clusters"),
+    )
+
+
+def _get_indic_files(db_path: Path) -> IndicFiles:
+    return IndicFiles(
+        active=JSONFile(db_path=db_path, file_name="indics_active"),
+        params=JSONFile(db_path=db_path, file_name="indics_params"),
+        clusters=JSONFile(db_path=db_path, file_name="indics_clusters"),
+    )
+
+
+def _get_backtest_files(db_path: Path) -> BacktestFiles:
+    return BacktestFiles(
+        results=JSONFile(db_path=db_path, file_name="backtest_results"),
+        returns=ParquetFile(db_path=db_path, file_name="returns_data"),
+        prices=ParquetFile(db_path=db_path, file_name="prices_data"),
+    )
