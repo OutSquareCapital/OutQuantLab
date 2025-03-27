41cc142461f537dff0037b2772beaba96618f91c
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-03-27 11:46:51 +0100

    updated data files and data queries: now use pathlib library rather than os. allow for more readable and concise code. datafiles now get a db path (directory) and file name at init. this move the responsibility of concret file retrieval to be handled by datafile directly. in turn, this allow the datafile class to explicitly handle missing files at loading, by returning default values, thus avoiding application crashing and manual file handling. before that, a missing file was silently processed. big issue! we still keep the advantage of having a single source of truth (dataqueries) when adding/removing a file for the application. this also remove the nested loop at dataqueries init. should be faster (albeit not tested)

--- outquantlab/database/data_file.py
+++ outquantlab/database/data_file.py
@@ -2,14 +2,29 @@ import json
 from abc import ABC, abstractmethod
 from pandas import DataFrame, read_parquet
 from typing import Any
+from pathlib import Path
 
-class DataFile[T:dict[str, Any] | DataFrame](ABC):
 
-    def __init__(self) -> None:
-        self.path: str = "default"
+class DataFile[T: dict[str, Any] | DataFrame](ABC):
+    extension: str
+
+    def __init__(self, db_path: Path, file_name: str) -> None:
+        self.path: Path = db_path / f"{file_name}{self.extension}"
+
+    def load(self, names: list[str] | None = None) -> T:
+        if not self.path.exists():
+            print(
+                f"File {self.path.name} is not found. Returning empty data.\n{self.path}"
+            )
+            return self._handle_missing_file()
+        return self._load_implementation(names=names)
 
     @abstractmethod
-    def load(self, names: list[str] | None = None) -> T:
+    def _load_implementation(self, names: list[str] | None = None) -> T:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _handle_missing_file(self) -> T:
         raise NotImplementedError
 
     @abstractmethod
@@ -17,16 +32,21 @@ class DataFile[T:dict[str, Any] | DataFrame](ABC):
         raise NotImplementedError
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.path})"
+        return f"{self.__class__.__name__} path: \n {self.path}"
 
 
 class JSONFile(DataFile[dict[str, Any]]):
-    def load(self, names: list[str] | None = None) -> dict[str, Any]:
+    extension = ".json"
+
+    def _load_implementation(self, names: list[str] | None = None) -> dict[str, Any]:
         with open(self.path, "r") as file:
             data = json.load(file)
         if names:
             return {key: data[key] for key in names if key in data}
         return data
+
+    def _handle_missing_file(self) -> dict[str, Any]:
+        return {}
 
     def save(self, data: dict[str, Any]) -> None:
         with open(self.path, "w") as file:
@@ -34,10 +54,15 @@ class JSONFile(DataFile[dict[str, Any]]):
 
 
 class ParquetFile(DataFile[DataFrame]):
-    def load(self, names: list[str] | None = None) -> DataFrame:
+    extension = ".parquet"
+
+    def _load_implementation(self, names: list[str] | None = None) -> DataFrame:
         if names:
             return read_parquet(self.path, engine="pyarrow", columns=names)
         return read_parquet(self.path, engine="pyarrow")
+
+    def _handle_missing_file(self) -> DataFrame:
+        return DataFrame()
 
     def save(self, data: DataFrame) -> None:
         data.to_parquet(self.path, engine="pyarrow", index=True)
--- outquantlab/database/data_queries.py
+++ outquantlab/database/data_queries.py
@@ -1,45 +1,26 @@
-import os
+from pathlib import Path
 
-from outquantlab.database.data_file import ParquetFile, JSONFile
-from dataclasses import dataclass, field
+from outquantlab.database.data_file import JSONFile, ParquetFile
 
-DB_NAME = "data"
+DB = "data"
 
-
-@dataclass
 class DataQueries:
-    assets_active: JSONFile = field(default_factory=JSONFile)
-    assets_clusters: JSONFile = field(default_factory=JSONFile)
-    indics_active: JSONFile = field(default_factory=JSONFile)
-    indics_params: JSONFile = field(default_factory=JSONFile)
-    indics_clusters: JSONFile = field(default_factory=JSONFile)
-    returns_data: ParquetFile = field(default_factory=ParquetFile)
-    prices_data: ParquetFile = field(default_factory=ParquetFile)
-    backtest_results: JSONFile = field(default_factory=JSONFile)
+    def __init__(self) -> None:
+        db_path: Path = self._get_db_path(db_name=DB)
+        self.assets_active: JSONFile = JSONFile(db_path=db_path, file_name="assets_active")
+        self.assets_clusters: JSONFile = JSONFile(db_path=db_path, file_name="assets_clusters")
+        self.indics_active: JSONFile = JSONFile(db_path=db_path, file_name="indics_active")
+        self.indics_params: JSONFile = JSONFile(db_path=db_path, file_name="indics_params")
+        self.indics_clusters: JSONFile = JSONFile(db_path=db_path, file_name="indics_clusters")
+        self.backtest_results: JSONFile = JSONFile(db_path=db_path, file_name="backtest_results")
+        self.returns_data: ParquetFile = ParquetFile(db_path=db_path, file_name="returns_data")
+        self.prices_data: ParquetFile = ParquetFile(db_path=db_path, file_name="prices_data")
 
-    def __post_init__(self) -> None:
-        current_file_path: str = os.path.abspath(__file__)
-        current_dir: str = os.path.dirname(current_file_path)
-        db_path: str = os.path.join(current_dir, DB_NAME)
-
-        for root, _, files in os.walk(db_path):
-            for file in files:
-                file_path: str = os.path.join(root, file)
-                file_name, _ = os.path.splitext(file)
-                self._assign_path(path=file_path, file_name=file_name)
-
-    def _assign_path(self, path: str, file_name: str) -> None:
-        try:
-            self[file_name].path = path
-        except KeyError:
-            raise ValueError(f"File {file_name} at \n {path} \n not found in DataQueries list")
+    def _get_db_path(self, db_name: str) -> Path:
+        current_file_path: Path = Path(__file__).resolve()
+        current_dir: Path = current_file_path.parent
+        return current_dir / db_name
 
     def check_data(self) -> None:
         for name, value in self.__dict__.items():
             print(f"{name}:\n {value}")
-
-    def __getitem__(self, key: str) -> ParquetFile | JSONFile:
-        return self.__dict__[key]
-
-    def __setitem__(self, key: str, value: ParquetFile | JSONFile) -> None:
-        self.__dict__[key] = value
