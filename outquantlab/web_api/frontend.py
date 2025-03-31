b6a89ce0eb518688168f83b2b3a418978c153f31
OutSquareCapital <outsquarecapital@gmail.com>
2025-03-31 16:14:57 +0200

    introduced new package encapsulating all web interactions (yahoo finance and frontend api). introduce all fastAPI related operations as well

---
+++ outquantlab/web_api/frontend.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+from outquantlab.typing_conventions import DataFrameDict, SeriesDict
+from enum import Enum
+
+class Server(Enum):
+    DATA = "/data"
+    PORT = 8000
+    IP = "127.0.0.1"
+    URL = f"http://{IP}:{PORT}{DATA}"
+
+
+_app = FastAPI()
+_data_store: dict[str, DataFrameDict | SeriesDict] = {}
+
+def start_server() -> None:
+    uvicorn.run(app=_app, host=Server.IP.value, port=Server.PORT.value)
+
+def send_data_to_server(id: str, results: DataFrameDict | SeriesDict) -> None:
+    _data_store[id] = results
+
+@_app.get(path=Server.DATA.value)
+def get_all_data() -> dict[str, DataFrameDict | SeriesDict]:
+    if not _data_store:
+        raise ValueError("No data available")
+    return _data_store
