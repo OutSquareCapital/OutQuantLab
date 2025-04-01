3b2548c0403d0a17f54297bbf20fb978625c9d66
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-01 17:06:44 +0200

    import hotfix

--- outquantlab/indicators/__init__.py
+++ outquantlab/indicators/__init__.py
@@ -1,6 +1,6 @@
 from outquantlab.indicators.indics_config import BaseIndic
 from outquantlab.indicators.indics_normalized import INDICATOR_REGISTRY
-from outquantlab.indicators.indics_config import DataArrays
+from outquantlab.indicators.data_arrays import DataArrays
 __all__: list[str] = [
     'BaseIndic',
     'INDICATOR_REGISTRY',
