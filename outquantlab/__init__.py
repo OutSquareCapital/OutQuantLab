16113f6ef51e137a39dcf6ffddb163553d962355
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-02 12:08:56 +0200

    changed the import convention for project. integrated DataFrameFloat and BacktestResults in namespace. will allow better typing and more flexibility (external data for stats plotting for example)

--- outquantlab/__init__.py
+++ outquantlab/__init__.py
@@ -1,5 +1,8 @@
 from outquantlab.main import OutQuantLab
-
+from outquantlab.core import BacktestResults
+from outquantlab.structures import DataFrameFloat
 __all__: list[str] = [
-    "OutQuantLab"
+    "OutQuantLab",
+    "BacktestResults",
+    "DataFrameFloat"
 ]
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from outquantlab import OutQuantLab
+import outquantlab as oql
 
 def use_example() -> None:
-    oql = OutQuantLab(refresh_data=False)
-    results = oql.run()
-    oql.stats.overall.sharpe_ratio.plot(data=results.assets)
-    oql.stats.rolling.drawdown.plot(data=results.portfolio, length=1250)
-    oql.save_config()
+    lab = oql.OutQuantLab(refresh_data=False)
+    results: oql.BacktestResults = lab.run()
+    lab.stats.overall.sharpe_ratio.plot(data=results.assets)
+    lab.stats.rolling.drawdown.plot(data=results.portfolio, length=1250)
+    lab.save_config()
 
 
 if __name__ == "__main__":
