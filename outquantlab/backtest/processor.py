be04718320e546544fdc4bbe9e6c3a1d517f8659
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-03 21:42:17 +0200

    clusters & portfolio constructions update: centralized it into core.clusters modules for the moment, to prepare a deeper refactor. WIP! also strong typing introduction. we'll see if maintained

--- outquantlab/backtest/processor.py
+++ outquantlab/backtest/processor.py
@@ -1,39 +1,30 @@
 from concurrent.futures import ThreadPoolExecutor
 from os import cpu_count
 
-from numpy import empty
-
-from outquantlab.core import BacktestConfig, BacktestResults
+from outquantlab.core import BacktestConfig, BacktestResults, aggregate_raw_returns
 from outquantlab.indicators import BaseIndic, DataArrays, get_data_arrays
-from outquantlab.structures import ArrayFloat, DataFrameFloat, Float32
+from outquantlab.structures import arrays, frames
 
 
 def process_backtest(
-    returns_df: DataFrameFloat,
+    returns_df: frames.DatedFloat,
     config: BacktestConfig,
 ) -> BacktestResults:
     processor: IndicatorsProcessor = IndicatorsProcessor(
         assets_nb=returns_df.shape[1],
         days_nb=returns_df.shape[0],
-        total_returns_streams=len(config.multi_index),
+        total_returns_streams=config.hierarchy.length,
     )
-    returns_df = DataFrameFloat(
+    returns_df = frames.DatedFloat(
         data=processor.process_strategies(
             indics_params=config.indics_params,
             data_arrays=get_data_arrays(pct_returns=returns_df.get_array()),
         ),
-        index=returns_df.dates,
-        columns=config.multi_index,
+        index=returns_df.get_index(),
+        columns=config.hierarchy.get_multi_index(),
     )
-    aggregate_raw_returns(returns_df=returns_df, results=config.backtest_results)
-    return config.backtest_results
+    return aggregate_raw_returns(returns_df=returns_df)
 
-def aggregate_raw_returns(returns_df: DataFrameFloat, results: BacktestResults) -> None:
-    for lvl in range(results.clusters_depth, 0, -1):
-        returns_df.get_portfolio_returns(grouping_levels=returns_df.columns.names[:lvl])
-        returns_df.clean_nans()
-        key_name: str = returns_df.columns.names[lvl - 1]
-        results[key_name] = returns_df
 
 class IndicatorsProcessor:
     def __init__(
@@ -44,14 +35,13 @@ class IndicatorsProcessor:
     ) -> None:
         self.assets_nb: int = assets_nb
         self.start_index: int = 0
-        self.results: ArrayFloat = empty(
-            shape=(days_nb, total_returns_streams),
-            dtype=Float32,
+        self.results: arrays.Float2D = arrays.empty(
+            length=days_nb, width=total_returns_streams
         )
 
     def fill_results_array(
         self,
-        results_list: list[ArrayFloat],
+        results_list: list[arrays.Float2D],
         params_nb: int,
     ) -> None:
         for i in range(params_nb):
@@ -61,12 +51,12 @@ class IndicatorsProcessor:
 
     def process_strategies(
         self, indics_params: list[BaseIndic], data_arrays: DataArrays
-    ) -> ArrayFloat:
+    ) -> arrays.Float2D:
         threads_nb: int = cpu_count() or 8
         with ThreadPoolExecutor(max_workers=threads_nb) as global_executor:
             for indic in indics_params:
                 try:
-                    results_list: list[ArrayFloat] = indic.process_params_parallel(
+                    results_list: list[arrays.Float2D] = indic.process_params_parallel(
                         data_arrays=data_arrays,
                         global_executor=global_executor,
                     )
--- outquantlab/core/config_state.py
+++ outquantlab/core/config_state.py
@@ -1,70 +1,20 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
-from pandas import MultiIndex
-
-from outquantlab.core.clusters import AssetsClusters, IndicsClusters
+from outquantlab.core.clusters import (
+    AssetsClusters,
+    AssetsClustersTuples,
+    ClustersHierarchy,
+    IndicsClusters,
+    IndicsClustersTuples,
+)
 from outquantlab.core.collections import AssetsConfig, IndicsConfig
 from outquantlab.indicators import BaseIndic
-from outquantlab.metrics import get_overall_mean
-from outquantlab.structures import DataFrameFloat
-
-
-@dataclass
-class BacktestResults:
-    assets_clusters: DataFrameFloat = field(default_factory=DataFrameFloat)
-    assets_subclusters: DataFrameFloat = field(default_factory=DataFrameFloat)
-    assets: DataFrameFloat = field(default_factory=DataFrameFloat)
-    indics_clusters: DataFrameFloat = field(default_factory=DataFrameFloat)
-    indics_subclusters: DataFrameFloat = field(default_factory=DataFrameFloat)
-    indics: DataFrameFloat = field(default_factory=DataFrameFloat)
-    params: DataFrameFloat = field(default_factory=DataFrameFloat)
-
-    @property
-    def clusters_levels(self) -> list[str]:
-        return [name for name in self.__dict__.keys()]
-
-    @property
-    def clusters_depth(self) -> int:
-        return len(self.__dict__.keys())
-
-    @property
-    def portfolio(self) -> DataFrameFloat:
-        return DataFrameFloat(
-            data=get_overall_mean(array=self.assets_clusters.get_array(), axis=1),
-            index=self.assets_clusters.dates,
-            columns=["portfolio"],
-        )
-
-    def __getitem__(self, key: str) -> DataFrameFloat:
-        value: DataFrameFloat = self.__dict__[key]
-        return value
-
-    def __setitem__(self, key: str, value: DataFrameFloat) -> None:
-        self.__dict__[key] = value
 
 
 @dataclass(slots=True)
 class BacktestConfig:
     indics_params: list[BaseIndic]
-    multi_index: MultiIndex
-    backtest_results: BacktestResults
-
-
-def _get_multi_index(
-    clusters_levels: list[str],
-    asset_tuples: list[tuple[str, ...]],
-    indics_tuples: list[tuple[str, ...]],
-) -> MultiIndex:
-    product_tuples: list[tuple[str, ...]] = [
-        (*asset_clusters, *indic_clusters)
-        for indic_clusters in indics_tuples
-        for asset_clusters in asset_tuples
-    ]
-
-    return MultiIndex.from_tuples(  # type: ignore
-        tuples=product_tuples,
-        names=[name for name in clusters_levels],
-    )
+    hierarchy: ClustersHierarchy
 
 
 @dataclass(slots=True)
@@ -81,23 +31,24 @@ class AppConfig:
             entities=self.indics_config.get_all_entities()
         )
         indics_params: list[BaseIndic] = self.indics_config.get_indics_params()
-        indics_tuples: list[tuple[str, ...]] = self.indics_clusters.get_clusters_tuples(
-            entities=indics_params
+        indics_tuples: list[IndicsClustersTuples] = (
+            self.indics_clusters.get_clusters_tuples(entities=indics_params)
         )
+
         self.assets_clusters.check_data_structure(
             entities=self.assets_config.get_all_entities()
         )
-        asset_tuples: list[tuple[str, ...]] = self.assets_clusters.get_clusters_tuples(
-            entities=self.assets_config.get_all_active_entities()
+        asset_tuples: list[AssetsClustersTuples] = (
+            self.assets_clusters.get_clusters_tuples(
+                entities=self.assets_config.get_all_active_entities()
+            )
         )
 
-        backtest_results: BacktestResults = BacktestResults()
+        hierarchy = ClustersHierarchy(
+            asset_tuples=asset_tuples,
+            indics_tuples=indics_tuples,
+        )
         return BacktestConfig(
-            backtest_results=backtest_results,
-            multi_index=_get_multi_index(
-                asset_tuples=asset_tuples,
-                indics_tuples=indics_tuples,
-                clusters_levels=backtest_results.clusters_levels,
-            ),
+            hierarchy=hierarchy,
             indics_params=indics_params,
         )
