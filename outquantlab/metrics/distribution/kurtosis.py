77e1a559725a51100e1dcb151e4056951188c84f
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-03 21:00:00 +0200

    using keyswords params rather than tuple for arrays module

--- outquantlab/metrics/distribution/kurtosis.py
+++ outquantlab/metrics/distribution/kurtosis.py
@@ -159,7 +159,7 @@ def remove_kurtosis_contribution(
 @njit
 def get_rolling_kurtosis(array: arrays.Float2D, length: int, min_length: int) -> arrays.Float2D:
     num_rows, num_cols = array.shape
-    output: arrays.Float2D = arrays.create_nan(shape=(num_rows, num_cols))
+    output: arrays.Float2D = arrays.create_nan(length=num_rows, width=num_cols)
 
     for col in prange(num_cols):
         (
--- outquantlab/structures/arrays/create.py
+++ outquantlab/structures/arrays/create.py
@@ -3,24 +3,24 @@ import numpy as np
 from outquantlab.structures.arrays.types import Float2D, Float32, Int2D, Nan
 
 
-def empty(shape: tuple[int, ...]) -> Float2D:
-    return np.empty(shape=shape, dtype=Float32)
+def empty(length: int, width: int) -> Float2D:
+    return np.empty(shape=(length, width), dtype=Float32)
 
 
 def empty_like(model: Float2D) -> Float2D:
     return np.empty_like(prototype=model, dtype=Float32)
 
 
-def full(shape: tuple[int, ...], fill_value: float) -> Float2D:
-    return np.full(shape=shape, fill_value=fill_value, dtype=Float32)
+def full(length: int, width: int, fill_value: float) -> Float2D:
+    return np.full(shape=(length, width), fill_value=fill_value, dtype=Float32)
 
 
 def full_like(model: Float2D, fill_value: float) -> Float2D:
     return np.full_like(a=model, fill_value=fill_value, dtype=Float32)
 
 
-def create_nan(shape: tuple[int, ...]) -> Float2D:
-    return np.full(shape=shape, fill_value=Nan, dtype=Float32)
+def create_nan(length: int, width: int) -> Float2D:
+    return np.full(shape=(length, width), fill_value=Nan, dtype=Float32)
 
 
 def nan_like(model: Float2D) -> Float2D:
