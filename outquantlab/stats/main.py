66363e5b7f92788ee0e420d44c105630c2b9eacc
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-06 16:32:21 +0200

    simplified stats class by directly giving default values to named tuples

--- outquantlab/stats/main.py
+++ outquantlab/stats/main.py
@@ -1,29 +1,36 @@
-from typing import NamedTuple
-import outquantlab.metrics as mt
 from dataclasses import dataclass, field
+from typing import NamedTuple
+
+import outquantlab.metrics as mt
+from outquantlab.apis import start_server
 from outquantlab.stats.processors import (
     AggregateProcessor,
+    EquityProcessor,
     RollingProcessor,
     SamplingProcessor,
     TableProcessor,
-    EquityProcessor
 )
 from outquantlab.structures import arrays, frames
-from outquantlab.apis import start_server
+
 
 class AggregateProcessorsRegistery(NamedTuple):
-    returns: AggregateProcessor
-    sharpe_ratio: AggregateProcessor
-    volatility: AggregateProcessor
-    drawdown: AggregateProcessor
-    skewness: AggregateProcessor
+    returns = AggregateProcessor(_func=mt.get_total_returns)
+    sharpe_ratio = AggregateProcessor(_func=mt.get_overall_sharpe_ratio)
+    volatility = AggregateProcessor(
+        _func=mt.get_overall_volatility_annualized,
+        _ascending=False,
+    )
+    drawdown = AggregateProcessor(_func=mt.get_overall_average_drawdown)
+    skewness = AggregateProcessor(
+        _func=mt.get_overall_monthly_skewness, _ascending=False
+    )
 
 
 class RollingProcessorsRegistery(NamedTuple):
-    sharpe_ratio: RollingProcessor
-    volatility: RollingProcessor
-    drawdown: RollingProcessor
-    skewness: RollingProcessor
+    sharpe_ratio = RollingProcessor(_func=mt.get_rolling_sharpe_ratio, _ascending=False)
+    volatility = RollingProcessor(_func=mt.get_rolling_volatility_annualized)
+    drawdown = RollingProcessor(_func=mt.get_rolling_drawdown)
+    skewness = RollingProcessor(_func=mt.get_rolling_skewness, _ascending=False)
 
 
 @dataclass(slots=True)
@@ -35,37 +42,17 @@ class Stats:
     equity: EquityProcessor = field(init=False)
 
     def __post_init__(self) -> None:
-        self.overall = AggregateProcessorsRegistery(
-            returns=AggregateProcessor(_func=mt.get_total_returns),
-            sharpe_ratio=AggregateProcessor(_func=mt.get_overall_sharpe_ratio),
-            volatility=AggregateProcessor(
-                _func=mt.get_overall_volatility_annualized,
-                _ascending=False,
-            ),
-            drawdown=AggregateProcessor(_func=mt.get_overall_average_drawdown),
-            skewness=AggregateProcessor(
-                _func=mt.get_overall_monthly_skewness, _ascending=False
-            ),
-        )
-        self.rolling = RollingProcessorsRegistery(
-            sharpe_ratio=RollingProcessor(
-                _func=mt.get_rolling_sharpe_ratio, _ascending=False
-            ),
-            volatility=RollingProcessor(_func=mt.get_rolling_volatility_annualized),
-            drawdown=RollingProcessor(_func=mt.get_rolling_drawdown),
-            skewness=RollingProcessor(_func=mt.get_rolling_skewness, _ascending=False),
-        )
+        self.overall = AggregateProcessorsRegistery()
+        self.rolling = RollingProcessorsRegistery()
         self.distribution = SamplingProcessor(
             _func=mt.get_returns_distribution, _ascending=False
         )
         self.correlation = TableProcessor(
             _func=mt.get_filled_correlation_matrix, _ascending=False
         )
-        self.equity = EquityProcessor(
-            _func=arrays.get_prices, _ascending=True
-        )
+        self.equity = EquityProcessor(_func=arrays.get_prices, _ascending=True)
 
-    def send_results(self, results:frames.DatedFloat) -> None:
+    def send_results(self, results: frames.DatedFloat) -> None:
         self.rolling.sharpe_ratio.send_to_api(data=results, length=1250)
         self.overall.sharpe_ratio.send_to_api(data=results)
         self.distribution.send_to_api(data=results, frequency=20)
