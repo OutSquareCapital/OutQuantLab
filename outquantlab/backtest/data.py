bd93991849336de1ec3ddb0ecf852d618a5710e9
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-07 18:14:09 +0200

    moved dataArrays into backtest package, changed it into a dataclass, added shift for pct returns

---
+++ outquantlab/backtest/data.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+from outquantlab.metrics import hv_composite
+from outquantlab.structures import arrays
+
+
+@dataclass(slots=True)
+class DataArrays:
+    pct_returns: arrays.Float2D
+    prices: arrays.Float2D = field(init=False)
+    log_returns: arrays.Float2D = field(init=False)
+    adjusted_returns: arrays.Float2D = field(init=False)
+
+    def __post_init__(self) -> None:
+        prices: arrays.Float2D = arrays.get_prices(returns=self.pct_returns)
+        log_returns: arrays.Float2D = arrays.get_log_returns(prices=prices)
+        hv: arrays.Float2D = hv_composite(returns_array=self.pct_returns)
+        self.prices = arrays.shift(original=prices)
+        self.log_returns = arrays.shift(original=log_returns)
+        self.adjusted_returns = get_volatility_adjusted_returns(
+            pct_returns_array=self.pct_returns, hv_array=hv
+        )
+        self.pct_returns = arrays.shift(original=self.pct_returns)
+
+
+def get_volatility_adjusted_returns(
+    pct_returns_array: arrays.Float2D,
+    hv_array: arrays.Float2D,
+    target_volatility: int = 25,
+) -> arrays.Float2D:
+    vol_adj_position_size_shifted: arrays.Float2D = arrays.shift(
+        original=target_volatility / hv_array
+    )
+    return pct_returns_array * vol_adj_position_size_shifted
--- outquantlab/indicators/__init__.py
+++ outquantlab/indicators/__init__.py
@@ -1,9 +1,6 @@
 from outquantlab.indicators.indics_config import BaseIndic
 from outquantlab.indicators.indics_normalized import INDICATOR_REGISTRY
-from outquantlab.indicators.data_arrays import DataArrays, get_data_arrays
 __all__: list[str] = [
     'BaseIndic',
-    'INDICATOR_REGISTRY',
-    'DataArrays',
-    'get_data_arrays',
+    'INDICATOR_REGISTRY'
 ]
