ee7ac111f0e22e9070299d1b145e42c961533329
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-07 18:19:01 +0200

    related to last commits.

--- outquantlab/metrics/correlation.py
+++ outquantlab/metrics/correlation.py
@@ -33,36 +33,3 @@ def get_cluster_structure(returns_array: arrays.Float2D, max_clusters: int) -> l
     distance_condensed: arrays.Float2D = squareform(distance_matrix, checks=False)
     linkage_matrix: arrays.Float2D = linkage(distance_condensed, method="ward")  # type: ignore
     return fcluster(linkage_matrix, max_clusters, criterion="maxclust")  # type: ignore
-
-
-def get_clusters(
-    returns_array: arrays.Float2D, asset_names: list[str], max_clusters: int
-) -> dict[str, list[str]]:
-    clusters_structure: list[int] = get_cluster_structure(
-        returns_array=returns_array, max_clusters=max_clusters
-    )
-
-    return _get_clusters_dict(
-        max_clusters=max_clusters, asset_names=asset_names, clusters_structure=clusters_structure
-    )
-
-
-def _get_clusters_dict(
-    max_clusters: int, asset_names: list[str], clusters_structure: list[int]
-) -> dict[str, list[str]]:
-    return {
-        str(object=cluster_id): _get_cluster_names(
-            cluster_id=cluster_id, asset_names=asset_names, clusters_structure=clusters_structure
-        )
-        for cluster_id in range(1, max_clusters + 1)
-    }
-
-
-def _get_cluster_names(
-    cluster_id: int, asset_names: list[str], clusters_structure: list[int]
-) -> list[str]:
-    return [
-        asset
-        for asset, cluster in zip(asset_names, clusters_structure)
-        if cluster == cluster_id
-    ]
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import outquantlab as oql
 
+
 def use_example() -> None:
-    lab = oql.OutQuantLab(refresh_data=False)
-    results: oql.BacktestResults = lab.run()
-    lab.stats.overall.sharpe_ratio.plot(data=results.assets)
-    lab.stats.rolling.drawdown.plot(data=results.portfolio, length=1250)
-    lab.save_config()
+    dbp = oql.DataBaseProvider(db_name="data")
+    config: oql.AppConfig = dbp.get_app_config()
+    lab = oql.OutQuantLab(
+        indics=config.indics_config.get_indics_params(),
+        returns_df=dbp.get_returns_data(app_config=config, new_data=False),
+    )
+    strats: oql.arrays.Float2D = lab.backtest()
+    results: oql.BacktestResults = lab.get_portfolio(data=strats)
+    stats = oql.Stats()
+    stats.overall.sharpe_ratio.plot(data=results.assets)
+    stats.rolling.drawdown.plot(data=results.portfolio, length=1250)
+    dbp.save_app_config(app_config=config)
 
 
 if __name__ == "__main__":
