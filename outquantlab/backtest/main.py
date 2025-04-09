8006f284afb4f9abd3e65aeefa0b623f45bf0556
OutSquareCapital <outsquarecapital@gmail.com>
2025-04-09 17:51:02 +0200

    re-introduced progress following with tqdm, and a more pleasant user experience when loading lots of indics

--- outquantlab/backtest/main.py
+++ outquantlab/backtest/main.py
@@ -2,23 +2,21 @@ from concurrent.futures import ThreadPoolExecutor
 
 from outquantlab.backtest.data import DataArrays
 from outquantlab.backtest.specs import BacktestError, BacktestSpecs
-from outquantlab.indicators import BaseIndic
+from outquantlab.indicators import GenericIndic
 from outquantlab.structures import arrays
 
 
 class Backtestor:
-    def __init__(self, pct_returns: arrays.Float2D, indics: list[BaseIndic]) -> None:
-        self.indics: list[BaseIndic] = indics
+    def __init__(self, pct_returns: arrays.Float2D, indics: list[GenericIndic]) -> None:
+        self.indics: list[GenericIndic] = indics
         self.data: DataArrays = DataArrays(pct_returns=pct_returns)
         self.specs = BacktestSpecs(
             pct_returns=pct_returns,
             indics=self.indics,
         )
 
-    def process_backtest(
-        self,
-    ) -> arrays.Float2D:
-        main_array: arrays.Float2D = self.specs.dims.get_main_array()
+    def process_backtest(self) -> arrays.Float2D:
+        main_array: arrays.Float2D = self.specs.get_main_array()
         with ThreadPoolExecutor(max_workers=self.specs.thread_nb) as global_executor:
             for indic in self.indics:
                 try:
@@ -33,16 +31,5 @@ class Backtestor:
                     )
 
                 except Exception as e:
-                    self.get_error(
-                        e=e,
-                        indic=indic,
-                    )
-
+                    raise BacktestError(indic=indic, e=e)
         return main_array
-
-    def get_error(self, e: Exception, indic: BaseIndic) -> BacktestError:
-        return BacktestError(
-            indic=indic,
-            specs=self.specs,
-            e=e,
-        )
--- outquantlab/backtest/specs.py
+++ outquantlab/backtest/specs.py
@@ -1,52 +1,56 @@
-from dataclasses import dataclass
 from os import cpu_count
 
-from outquantlab.indicators import BaseIndic
+from outquantlab.indicators import GenericIndic
 from outquantlab.structures import arrays
-
-
-@dataclass(slots=True, frozen=True)
-class Dimensions:
-    assets: int
-    days: int
-    indics: int
-    params: int
-
-    @property
-    def total(self) -> int:
-        return self.assets * self.params
-
-    def get_main_array(self) -> arrays.Float2D:
-        return arrays.create_empty(length=self.days, width=self.total)
+from tqdm import tqdm
 
 
 class BacktestSpecs:
-    def __init__(self, pct_returns: arrays.Float2D, indics: list[BaseIndic]) -> None:
+    def __init__(self, pct_returns: arrays.Float2D, indics: list[GenericIndic]) -> None:
         self.thread_nb: int = cpu_count() or 8
         self.current_index: int = 0
-        self.dims = Dimensions(
-            assets=pct_returns.shape[1],
-            days=pct_returns.shape[0],
-            indics=len(indics),
-            params=sum([indic.params.quantity for indic in indics]),
-        )
+        self.assets: int = pct_returns.shape[1]
+        self.days: int = pct_returns.shape[0]
+        self.indics: int = len(indics)
+        self.params: int = sum([indic.quantity for indic in indics])
+        self.total: int = self.assets * self.params
+        print(self.get_stats())
+        self.progress_bar = tqdm(total=self.total, desc="Backtest Progress")
+
+    def get_main_array(self) -> arrays.Float2D:
+        return arrays.create_empty(length=self.days, width=self.total)
 
     def fill_main_array(
         self, main_array: arrays.Float2D, results_list: list[arrays.Float2D]
     ) -> None:
         for i in range(len(results_list)):
-            end_index: int = self.current_index + self.dims.assets
+            end_index: int = self.current_index + self.assets
             main_array[:, self.current_index : end_index] = results_list[i]
+            self.update_progress()
             self.current_index = end_index
+
+    def update_progress(self) -> None:
+        self.progress_bar.update(self.assets)
+        self.progress_bar.refresh()
+        if self.current_index >= self.total:
+            self.progress_bar.close()
+
+    def get_stats(self) -> str:
+        return (
+            f"Backtest Numbers Statistics:\n"
+            f"  Threads: {self.thread_nb},\n"
+            f"  Days: {self.days},\n"
+            f"  Assets: {self.assets},\n"
+            f"  Indics: {self.indics},\n"
+            f"  Params: {self.params},\n"
+            f"  Total Nb of strategies: {self.total}\n"
+        )
 
 
 class BacktestError(Exception):
     def __init__(
         self,
-        indic: BaseIndic,
-        specs: BacktestSpecs,
+        indic: GenericIndic,
         e: Exception,
     ) -> None:
-        super().__init__(
-            f"Error during backtest.\n Issue: {e} \n Specs:\n {specs}\n Indicator:\n {indic}"
-        )
+        super().__init__(f"Error during backtest.\n Issue: {e} \n Indicator:\n {indic}")
