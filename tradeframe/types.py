e5ad8e6340ce1add71ecd693448e1fa6e99db092
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-04-16 17:53:21 +0200

    added base class for config

--- tradeframe/types.py
+++ tradeframe/types.py
@@ -13,17 +13,29 @@ class Category(Protocol):
 
 class ColumnsIDs(StrEnum):
     INDEX = "Index"
-    VALUES = "Values"
     NAMES = "Names"
     DATE = "Date"
 
-
 @dataclass(slots=True)
-class SeriesConfig:
+class _BaseConfig:
     index_col: str
     index_type: pl.DataType
-    values_col: str = field(init=False, default=ColumnsIDs.VALUES)
     values_type: pl.Float32 = field(init=False, default=pl.Float32())
+
+    def get_index(self, data: list[str] | nq.Int1D) -> pl.Series:
+        return pl.Series(name=self.index_col, values=data, dtype=self.index_type)
+
+    def create_empty(self) -> pl.DataFrame:
+        return pl.DataFrame(
+            data={
+                self.index_col: pl.Series(name=self.index_col, values=[]),
+            }
+        )
+
+
+@dataclass(slots=True)
+class SeriesConfig(_BaseConfig):
+    values_col: str = field(init=False, default="Values")
 
     @property
     def schema(self) -> dict[str, Any]:
@@ -31,9 +43,6 @@ class SeriesConfig:
             self.index_col: self.index_type,
             self.values_col: self.values_type,
         }
-
-    def get_index(self, data: list[str] | nq.Int1D) -> pl.Series:
-        return pl.Series(name=self.index_col, values=data, dtype=self.index_type)
 
     def get_data(self, array: nq.Float2D) -> pl.Series:
         return pl.Series(
@@ -48,33 +57,18 @@ class SeriesConfig:
 
 
 @dataclass(slots=True)
-class FrameConfig:
-    index_col: str
-    index_type: pl.DataType
-    values_type: pl.Float32 = field(init=False, default=pl.Float32())
-
+class FrameConfig(_BaseConfig):
     @property
     def schema(self) -> dict[str, Any]:
         return {self.index_col: self.index_type}
-
-    def create_empty(self) -> pl.DataFrame:
-        return pl.DataFrame(
-            data={
-                self.index_col: pl.Series(name=self.index_col, values=[]),
-            }
-        )
 
     def get_data(self, array: nq.Float2D, asset_names: list[str]) -> pl.DataFrame:
         return pl.from_numpy(data=array, orient="row", schema=asset_names).fill_nan(
             value=None
         )
 
-    def get_index(self, data: list[str] | nq.Int1D) -> pl.Series:
-        return pl.Series(
-            name=self.index_col,
-            values=data,
-            dtype=self.index_type,
-        )
-
     def create(self, values: pl.DataFrame, index: pl.Series) -> pl.DataFrame:
         return values.with_columns(index)
+
+    def format_index(self, data: pl.DataFrame) -> pl.DataFrame:
+        return data.with_columns(pl.col(name=self.index_col).cast(dtype=self.index_type))
