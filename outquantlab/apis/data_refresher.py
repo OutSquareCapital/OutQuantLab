1e576458c73e2f48663d7d64676a8eb5bd6b314e
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-04-17 20:00:46 +0200

    since tradeframe is now not responsible at all for database interactions, yahoo API is now totally decoupled from it

--- outquantlab/apis/data_refresher.py
+++ outquantlab/apis/data_refresher.py
@@ -1,36 +1,48 @@
 import yfinance as yf  # type: ignore
 from pandas import DataFrame
-
 import numquant as nq
-import tradeframe as tf
+from enum import StrEnum
+import polars as pl
+
+class YFConstants(StrEnum):
+    CLOSE = "Close"
+    TEST = "AAPL"
+    PERIOD = "1d"
+    DATE = "Date"
 
 class YahooData:
-    def fetch_data(self, assets: list[str]) -> tf.FrameDated:
-        self._test_connection(asset=assets[0])
-        return self._get_yf_data(assets=assets)
-
-    def _get_yf_data(self, assets: list[str]) -> tf.FrameDated:
-        prices: tf.FrameDated = self._get_prices_data(assets=assets)
-        return tf.FrameDated.create_from_np(
-            data=nq.arrays.get_pct_returns(prices=prices.get_array()),
-            asset_names=prices.get_names(),
-            dates=prices.index,
+    def __init__(self, connect:bool = False) -> None:
+        if connect:
+            try:
+                yf.Ticker(ticker=YFConstants.TEST).history(period=YFConstants.PERIOD)  # type: ignore
+            except Exception as e:
+                raise ConnectionError("Failed to connect to Yahoo Finance") from e
+        
+    def refresh_data(self, assets: list[str]) -> None:
+        prices: pl.DataFrame = self._get_prices_data(assets=assets)
+        dates: pl.DataFrame = prices.select(YFConstants.DATE)
+        prices_data: pl.DataFrame = prices.drop([YFConstants.DATE])
+        prices_values: nq.Float2D = prices_data.to_numpy()
+        asset_names: list[str] = prices_data.columns
+        self.prices: pl.DataFrame = pl.DataFrame(
+            data=prices_values,
+            schema=asset_names
+        )
+        self.returns: pl.DataFrame = pl.DataFrame(
+            data=nq.arrays.get_pct_returns(prices=prices_values),
+            schema=asset_names
         )
 
+        self.dates = pl.DataFrame(data=dates)
 
-    def _test_connection(self, asset: str) -> None:
-        try:
-            yf.Ticker(ticker=asset).history(period="1d")  # type: ignore
-        except Exception as e:
-            raise ConnectionError("Failed to connect to Yahoo Finance") from e
 
-    def _get_prices_data(self, assets: list[str]) -> tf.FrameDated:
+    def _get_prices_data(self, assets: list[str]) -> pl.DataFrame:
         data: DataFrame | None = yf.download(  # type: ignore
             tickers=assets,
-            interval="1d",
+            interval=YFConstants.PERIOD,
             auto_adjust=True,
             progress=False,
         )
         if data is None:
             raise ValueError(f"No data returned from Yahoo Finance, from {assets}")
-        return tf.FrameDated.create_from_pd(pd_df=data["Close"]) # type: ignore
+        return pl.from_pandas(data=data[YFConstants.CLOSE], include_index=True) # type: ignore
