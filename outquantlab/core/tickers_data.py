a50c1a47452dc38b6c6f17996b30748d3552050e
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-04-19 15:35:27 +0200

    better api for core data containers

--- outquantlab/core/tickers_data.py
+++ outquantlab/core/tickers_data.py
@@ -1,31 +1,39 @@
 import polars as pl
+
 import numquant as nq
 import tradeframe as tf
 
 
 class DatesIndexer:
-    def __init__(self, dates: pl.DataFrame) -> None:
-        daily_index: nq.Int1D = nq.arrays.get_index(array=dates.to_numpy())
+    _DATE = "Date"
+    _INDEX = "Index"
+
+    def __init__(self, dates_df: pl.DataFrame) -> None:
+        daily_index: nq.Int1D = nq.arrays.get_index(array=dates_df.to_numpy())
+        index = pl.Series(name=self._INDEX, values=daily_index, dtype=pl.Int32())
+        dates = pl.Series(
+            name=self._DATE, values=dates_df.select(pl.col(self._DATE)), dtype=pl.Date()
+        )
         self.data = pl.DataFrame(
             data={
-                "Date": pl.Series(name="Date", values=dates, dtype=pl.Date()),
-                "Index": pl.Series(name="Index", values=daily_index, dtype=pl.Int32()),
+                self._DATE: dates,
+                self._INDEX: index,
             }
         )
 
 
 class TickersData:
     def __init__(
-        self, dates: pl.DataFrame, prices: pl.DataFrame, returns: pl.DataFrame
+        self, prices: pl.DataFrame, returns: pl.DataFrame, dates: pl.DataFrame
     ) -> None:
-        self.dates = DatesIndexer(dates=dates)
-        self.prices: pl.DataFrame = prices
-        self.returns: pl.DataFrame = returns
-
-    def get_returns_data(self) -> tf.FrameVertical:
-        array: nq.Float2D = nq.arrays.convert(data=self.returns.to_numpy())
-        asset_names: list[str] = self.returns.columns
-        data: tf.FrameVertical = tf.FrameVertical.create_from_np(
-            data=array, asset_names=asset_names
+        self.dates = DatesIndexer(dates_df=dates)
+        asset_names: list[str] = returns.columns
+        prices_array: nq.Float2D = nq.arrays.convert(data=prices.to_numpy())
+        self.prices: tf.FrameVertical = tf.FrameVertical.create_from_np(
+            data=prices_array, asset_names=asset_names
         )
-        return data
+        returns_array: nq.Float2D = nq.arrays.convert(data=returns.to_numpy())
+
+        self.returns: tf.FrameVertical = tf.FrameVertical.create_from_np(
+            data=returns_array, asset_names=asset_names
+        )
